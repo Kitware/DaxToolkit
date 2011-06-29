7716f52fda474f7c66e3be1a8136f89e5a83d82e
Kenneth Moreland <kmorel@sandia.gov>
2011-06-29 10:55:05 -0600

    Give names to structs for DaxVector3 and DaxVector4 typedefs.
    CUDA 3.2 seems to have problems with something like
    
      typedef struct { ... } DaxVector3;
    
    It wants the struct to have a name (in addition to a typedef for
    the struct).  Thus, I just changed the declarations to look more
    like
    
      typedef struct DaxVector3Struct { ... } DaxVector3;
    
    (Note, I've left out the alignment in these examples for clarity.)

--- Common/daxTypes.h
+++ Common/daxTypes.h
@@ -10,11 +10,17 @@
 
 /// Alignment requirements are prescribed by CUDA on device (Table B-1 in NVIDIA
 /// CUDA C Programming Guide 4.0)
+
 typedef float DaxScalar __attribute__ ((aligned (4)));
-typedef struct { float x; float y; float z; } DaxVector3 __attribute__
-((aligned(4)));
-typedef struct { float x; float y; float z; float w; } DaxVector4 __attribute__
-((aligned(16)));
+
+typedef struct DaxVector3Struct {
+  float x; float y; float z;
+} DaxVector3 __attribute__ ((aligned(4)));
+
+typedef struct DaxVector4Struct {
+  float x; float y; float z; float w;
+} DaxVector4 __attribute__ ((aligned(16)));
 
 typedef int DaxId __attribute__ ((aligned(4)));
+
 #endif
