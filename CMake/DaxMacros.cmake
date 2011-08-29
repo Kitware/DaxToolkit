329cc4f84f00fdbd6b8ea0076fd84b39afe111d4
Kenneth Moreland <kmorel@sandia.gov>
2011-08-28 21:05:36 -0600

    Add header files to header test build executable.
    This makes the header files show up in the IDE, which in turn makes
    editing them much more convienient.

--- CMake/DaxMacros.cmake
+++ CMake/DaxMacros.cmake
@@ -7,15 +7,21 @@
 #=========================================================================
 
 function (add_header_build_test name dir_prefix)
-  set (HEADERS)
+  set(HEADERS)
+  set(hfiles)
   foreach (hdr ${ARGN})
-    set (HEADERS "${HEADERS}#include \"${dir_prefix}/${hdr}\"\n")
+    set(HEADERS "${HEADERS}#include \"${dir_prefix}/${hdr}\"\n")
+    set(hfiles ${hfiles} ${Dax_SOURCE_DIR}/${dir_prefix}/${hdr})
   endforeach()
   
   configure_file(${Dax_SOURCE_DIR}/CMake/TestBuild.cxx.in
     ${CMAKE_CURRENT_BINARY_DIR}/TestBuild${name}.cxx
     @ONLY)
   add_executable(TestBuild${name}
-    ${CMAKE_CURRENT_BINARY_DIR}/TestBuild${name}.cxx)
+    ${CMAKE_CURRENT_BINARY_DIR}/TestBuild${name}.cxx
+    ${hfiles})
+  set_source_files_properties(${hfiles}
+    PROPERTIES HEADER_FILE_ONLY TRUE
+    )
 endfunction()
 
