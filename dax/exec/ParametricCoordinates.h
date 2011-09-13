217c821d64b72bf98eaa8051744d8ecd6cf603af
Kenneth Moreland <kmorel@sandia.gov>
2011-09-13 16:20:22 -0600

    Add functions for converting between parametric and world coords

---
+++ dax/exec/ParametricCoordinates.h
@@ -0,0 +1,52 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+===========================================================================*/
+
+#ifndef __dax_exec_ParametricCoordinates_h
+#define __dax_exec_ParametricCoordinates_h
+
+#include <dax/Types.h>
+#include <dax/exec/Cell.h>
+#include <dax/exec/Field.h>
+
+#include <dax/exec/WorkMapCell.h>
+namespace dax {
+namespace exec {
+
+//-----------------------------------------------------------------------------
+template<class WorkType>
+DAX_EXEC_EXPORT dax::Vector3 parametricCoordinatesToWorldCoordinates(
+  const WorkType &work,
+  const dax::exec::CellVoxel &cell,
+  const dax::exec::FieldCoordinates &coordField,
+  const dax::Vector3 pcoords)
+{
+  dax::Vector3 spacing = cell.GetSpacing();
+  dax::Vector3 cellOffset = spacing * pcoords;
+
+  dax::Vector3 minCoord = work.GetFieldValue(coordField, 0);
+  return cellOffset + minCoord;
+}
+
+template<class WorkType>
+DAX_EXEC_EXPORT dax::Vector3 worldCoordinatesToParametricCoordinates(
+  const WorkType &work,
+  const dax::exec::CellVoxel &cell,
+  const dax::exec::FieldCoordinates &coordField,
+  const dax::Vector3 wcoords)
+{
+  dax::Vector3 minCoord = work.GetFieldValue(coordField, 0);
+  dax::Vector3 cellOffset = wcoords - minCoord;
+
+  dax::Vector3 spacing = cell.GetSpacing();
+  return cellOffset / spacing;
+}
+
+}
+}
+
+#endif //__dax_exec_ParametricCoordinates_h
---
+++ dax/exec/UnitTestParametricCoordinates.cxx
@@ -0,0 +1,137 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+===========================================================================*/
+#include <dax/exec/ParametricCoordinates.h>
+
+#include <dax/exec/WorkMapCell.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <algorithm>
+#include <vector>
+
+
+#define TEST_FAIL(msg)                                  \
+  {                                                     \
+    std::stringstream error;                            \
+    error << __FILE__ << ":" << __LINE__ << std::endl;  \
+    error msg;                                          \
+    throw error.str();                                  \
+  }
+
+template<class WorkType, class CellType>
+static void CompareCoordinates(const WorkType &work,
+                               const CellType &cell,
+                               const dax::exec::FieldCoordinates &coordField,
+                               dax::Vector3 truePCoords,
+                               dax::Vector3 trueWCoords)
+{
+  dax::Vector3 computedWCoords
+      = dax::exec::parametricCoordinatesToWorldCoordinates(work,
+                                                           cell,
+                                                           coordField,
+                                                           truePCoords);
+  if (computedWCoords != trueWCoords)
+    {
+    TEST_FAIL(<< "Computed wrong world coords from parametric coords.");
+    }
+
+  dax::Vector3 computedPCoords
+      = dax::exec::worldCoordinatesToParametricCoordinates(work,
+                                                           cell,
+                                                           coordField,
+                                                           trueWCoords);
+  if (computedPCoords != truePCoords)
+    {
+    TEST_FAIL(<< "Computed wrong parametric coords from world coords.");
+    }
+}
+
+static void TestPCoordsVoxel(
+  const dax::exec::WorkMapCell<dax::exec::CellVoxel> &work,
+  const dax::exec::FieldCoordinates &coordField)
+{
+  const dax::Vector3 cellVertexToParametricCoords[8] = {
+    { 0, 0, 0 },
+    { 1, 0, 0 },
+    { 1, 1, 0 },
+    { 0, 1, 0 },
+    { 0, 0, 1 },
+    { 1, 0, 1 },
+    { 1, 1, 1 },
+    { 0, 1, 1 }
+  };
+  const dax::exec::CellVoxel &cell = work.GetCell();
+
+  // Check the coordinates at all vertices
+  for (dax::Id vertexIndex = 0; vertexIndex < 8; vertexIndex++)
+    {
+    dax::Vector3 truePCoords = cellVertexToParametricCoords[vertexIndex];
+    dax::Vector3 trueWCoords = work.GetFieldValue(coordField, vertexIndex);
+    CompareCoordinates(work, cell, coordField, truePCoords, trueWCoords);
+    }
+
+  dax::Vector3 centerCoords = dax::make_Vector3(0.0, 0.0, 0.0);
+  for (dax::Id vertexIndex = 0; vertexIndex < 8; vertexIndex++)
+    {
+    centerCoords = centerCoords + work.GetFieldValue(coordField, vertexIndex);
+    }
+  centerCoords = (1.0/8.0) * centerCoords;
+  CompareCoordinates(
+        work, cell, coordField, dax::make_Vector3(0.5,0.5,0.5), centerCoords);
+}
+
+static void TestPCoordsVoxel()
+{
+  std::cout << "Testing TestPCoords<CellVoxel>" << std::endl;
+
+  dax::internal::StructureUniformGrid gridstruct;
+
+  dax::internal::DataArray<dax::Vector3> dummyArray;
+  dax::exec::FieldCoordinates coordField(dummyArray);
+
+  gridstruct.Origin = dax::make_Vector3(0, 0, 0);
+  gridstruct.Spacing = dax::make_Vector3(1, 1, 1);
+  gridstruct.Extent.Min = dax::make_Id3(0, 0, 0);
+  gridstruct.Extent.Max = dax::make_Id3(10, 10, 10);
+  for (dax::Id flatIndex = 0; flatIndex < 1000; flatIndex++)
+    {
+    dax::exec::WorkMapCell<dax::exec::CellVoxel> work(gridstruct, flatIndex);
+    TestPCoordsVoxel(work, coordField);
+    }
+
+  gridstruct.Origin = dax::make_Vector3(0, 0, 0);
+  gridstruct.Spacing = dax::make_Vector3(1, 1, 1);
+  gridstruct.Extent.Min = dax::make_Id3(5, -9, 3);
+  gridstruct.Extent.Max = dax::make_Id3(15, 6, 13);
+  dax::exec::WorkMapCell<dax::exec::CellVoxel> work(gridstruct, 0);
+  for (dax::Id flatIndex = 0; flatIndex < 1500; flatIndex++)
+    {
+    work.SetCellIndex(flatIndex);
+    TestPCoordsVoxel(work, coordField);
+    }
+}
+
+int UnitTestParametricCoordinates(int, char *[])
+{
+  try
+    {
+    TestPCoordsVoxel();
+    }
+  catch (std::string error)
+    {
+    std::cout
+        << "Encountered error: " << std::endl
+        << error << std::endl;
+    return 1;
+    }
+
+  return 0;
+}
