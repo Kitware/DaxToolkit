c7ce2451f7052a829d523ea1dfd73be3b08739ee
Kenneth Moreland <kmorel@sandia.gov>
2011-09-26 17:20:40 -0600

    Removing KernelArgument objects.
    I'm pretty sure this will change enough to justify just redoing
    the whole thing.

--- dax/cuda/exec/ExecutionEnvironment.h
+++ dax/cuda/exec/ExecutionEnvironment.h
@@ -5,13 +5,8 @@
   PURPOSE.  See the above copyright notice for more information.
 
 ===========================================================================*/
-// Includes for device code.
-#include <dax/exec/Cell.h>
-#include <dax/exec/Field.h>
-#include <dax/exec/Work.h>
-
 
 #define DAX_WORKLET __device__
 //#define DAX_IN const
-#define DAX_IN 
+#define DAX_IN
 #define DAX_OUT
