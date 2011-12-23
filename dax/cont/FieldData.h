9e20071d71bbde3afe4ee98d9b621af482910d3b
Robert Maynard <robert.maynard@kitware.com>
2011-12-23 18:33:41 -0500

    Added a template way to get arrays from FieldData.
    Using .get(T(),name) to programmatically get the right array
    where T is the primitive type ( scalar, vector3, etc ).
    If it is called with an unsupported type it will fail at compile time.

--- dax/cont/FieldData.h
+++ dax/cont/FieldData.h
@@ -8,6 +8,8 @@
 #ifndef __dax_cont_FieldData_h
 #define __dax_cont_FieldData_h
 
+#include <boost/static_assert.hpp>
+
 #include <dax/cont/internal/Object.h>
 #include <dax/cont/internal/FieldContainer.h>
 #include <dax/cont/internal/ArrayContainer.h>
@@ -57,24 +59,24 @@ public:
                 const Vec4Container& container)
     {return Vec4Field.add(name,container);}
 
-  IdContainer& getId(const std::string &name)
+  IdContainer& id(const std::string &name)
     { return IdField.get(name); }
-  const IdContainer& getId(const std::string &name) const
+  const IdContainer& id(const std::string &name) const
     { return IdField.get(name); }
 
-  ScalarContainer& getScalar(const std::string &name)
+  ScalarContainer& scalar(const std::string &name)
   { return ScalarField.get(name); }
-  const ScalarContainer& getScalar(const std::string &name) const
+  const ScalarContainer& scalar(const std::string &name) const
   { return ScalarField.get(name); }
 
-  Vec3Container& getVector3(const std::string &name)
+  Vec3Container& vector3(const std::string &name)
   { return Vec3Field.get(name); }
-  const Vec3Container& getVector3(const std::string &name) const
+  const Vec3Container& vector3(const std::string &name) const
   { return Vec3Field.get(name); }
 
-  Vec4Container& getVector4(const std::string &name)
+  Vec4Container& vector4(const std::string &name)
   { return Vec4Field.get(name); }
-  const Vec4Container& getVector4(const std::string &name) const
+  const Vec4Container& vector4(const std::string &name) const
   { return Vec4Field.get(name); }
 
   bool removeId(const std::string &name)
@@ -85,7 +87,112 @@ public:
   { return Vec3Field.remove(name); }
   bool removeVector4(const std::string &name)
   { return Vec4Field.remove(name); }
+
+  //Used to get the right field container based
+  //on type ( id, scalar, vector3, or vector4 ).
+  template <typename T>
+  dax::cont::internal::ArrayContainer<T>& get(T t, const std::string &name);
+
+  //Used to get the right field container based
+  //on type ( id, scalar, vector3, or vector4 ).
+  template <typename T>
+  const dax::cont::internal::ArrayContainer<T>& get(T t,
+                                                const std::string &name) const;
 };
+
+
+//------------------------------------------------------------------------------
+template <typename T>
+inline dax::cont::internal::ArrayContainer<T>& FieldData::get(
+    T,const std::string& name)
+{
+  //we specialize id,scalar,vector3,and vector4 to call the right
+  //internal containers get method. If a user attempts with any other
+  //we fail at compile time
+
+  //nvcc or gcc 4.4 tries to evaluate STATIC_ASSERT. So a workaround
+  //is for the assert to be dependent on a template parameter
+  BOOST_STATIC_ASSERT_MSG(sizeof(T)==0,NotAValidFieldType);
+
+  //this return can never be hit
+  return dax::cont::internal::ArrayContainer<T>();
+}
+
+//------------------------------------------------------------------------------
+template <typename T>
+inline const dax::cont::internal::ArrayContainer<T>& FieldData::get(
+    T,const std::string& name) const
+{
+  //nvcc or gcc 4.4 tries to evaluate STATIC_ASSERT. So a workaround
+  //is for the assert to be dependent on a template parameter
+  BOOST_STATIC_ASSERT_MSG(sizeof(T)==0,NotAValidFieldType);
+
+  //this return can never be hit
+  return dax::cont::internal::ArrayContainer<T>();
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline dax::cont::internal::ArrayContainer<dax::Id>& FieldData::get(
+    dax::Id,const std::string& name)
+{
+  return this->id(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline dax::cont::internal::ArrayContainer<dax::Scalar>& FieldData::get(
+    dax::Scalar,const std::string& name)
+{
+  return this->scalar(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline dax::cont::internal::ArrayContainer<dax::Vector3>& FieldData::get(
+    dax::Vector3,const std::string& name)
+{
+  return this->vector3(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline dax::cont::internal::ArrayContainer<dax::Vector4>& FieldData::get(
+    dax::Vector4, const std::string& name)
+{
+  return this->vector4(name);
+}
+//------------------------------------------------------------------------------
+template <>
+inline const dax::cont::internal::ArrayContainer<dax::Id>& FieldData::get(
+    dax::Id,const std::string& name) const
+{
+  return this->id(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline const dax::cont::internal::ArrayContainer<dax::Scalar>& FieldData::get(
+    dax::Scalar,const std::string& name) const
+{
+  return this->scalar(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline const dax::cont::internal::ArrayContainer<dax::Vector3>& FieldData::get(
+    dax::Vector3,const std::string& name) const
+{
+  return this->vector3(name);
+}
+
+//------------------------------------------------------------------------------
+template <>
+inline const dax::cont::internal::ArrayContainer<dax::Vector4>& FieldData::get(
+    dax::Vector4, const std::string& name) const
+{
+  return this->vector4(name);
+}
 } }
 
 #endif // __dax_cont_FieldData_h
