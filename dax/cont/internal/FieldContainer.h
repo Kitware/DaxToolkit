2ce97f9ed2be2fffe18e0c6a0e7462554472c385
Robert Maynard <robert.maynard@kitware.com>
2011-12-28 09:53:36 -0500

    Cleaned up the field container to return an invalid array when not found.

--- dax/cont/internal/FieldContainer.h
+++ dax/cont/internal/FieldContainer.h
@@ -9,36 +9,73 @@
 #define __dax_cont_internal_FieldContainer_h
 
 #include <dax/cont/internal/Object.h>
+
 #include <dax/cont/Array.h>
 #include <dax/cont/internal/ArrayContainer.h>
+
+#include <boost/throw_exception.hpp>
 #include <map>
 
-
 namespace dax { namespace cont { namespace internal {
+
+struct exception_base: virtual std::exception, virtual boost::exception { };
+struct array_not_found: virtual exception_base { };
+
 template<typename T>
 class FieldContainer : public dax::cont::internal::Object
 {
 private:
-  typedef dax::cont::ArrayPtr<T> ContArray;
-  typedef dax::cont::internal::ArrayContainer<T> TArray;
-  typedef std::pair<std::string,TArray> MapPair;
-  std::map<std::string,TArray> Container;
+  typedef dax::cont::ArrayPtr<T> ControlArray;
+  typedef dax::cont::internal::ArrayContainer<T> ArrayContainer;
+  typedef std::map<std::string,ArrayContainer > Map;
+  typedef std::pair<std::string,ArrayContainer> MapPair;
+  typedef typename Map::iterator MapIterator;
+  typedef typename Map::const_iterator ConstMapIterator;
+
+
+  Map Container;
+  //we return this when we can't find a valid array
+  ArrayContainer InvalidArray;
 
 public:
-  TArray& get(const std::string &name)
-    {return Container.find(name)->second;}
-  const TArray& get(const std::string &name) const
-    {return Container.find(name)->second;}
+  ArrayContainer& get(const std::string &name);
+  const ArrayContainer& get(const std::string &name) const;
 
-  bool add(const std::string &name, const TArray &t);
-  bool add(const std::string &name, ContArray ca);
+  bool add(const std::string &name, const ArrayContainer &t);
+  bool add(const std::string &name, ControlArray ca);
   bool exists(const std::string &name) const;
   bool remove(const std::string &name);
 };
 
 //------------------------------------------------------------------------------
 template<typename T>
-bool FieldContainer<T>::add(const std::string &name, const TArray &t)
+FieldContainer<T>::ArrayContainer& FieldContainer<T>::get(
+    const std::string &name)
+{
+  MapIterator it = this->Container.find(name);
+  if(it==this->Container.end())
+    {
+    return this->InvalidArray;
+    }
+  return it->second;
+}
+
+//------------------------------------------------------------------------------
+template<typename T>
+const FieldContainer<T>::ArrayContainer& FieldContainer<T>::get(
+    const std::string &name) const
+{
+  ConstMapIterator it = this->Container.find(name);
+  if(it==this->Container.end())
+    {
+    return this->InvalidArray;
+    }
+  return it->second;
+}
+
+//------------------------------------------------------------------------------
+template<typename T>
+bool FieldContainer<T>::add(const std::string &name, const ArrayContainer &t)
 {
   if(this->exists(name))
     {
@@ -50,9 +87,9 @@ bool FieldContainer<T>::add(const std::string &name, const TArray &t)
 
 //------------------------------------------------------------------------------
 template<typename T>
-bool FieldContainer<T>::add(const std::string &name, ContArray ca)
+bool FieldContainer<T>::add(const std::string &name, ControlArray ca)
 {
-  TArray container;
+  ArrayContainer container;
   container.setArrayControl(ca);
   return this->add(name,container);
 }
