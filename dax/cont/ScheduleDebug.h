66ebe1c9c86340ba3f261239cd7ef133ac813587
Kenneth Moreland <kmorel@sandia.gov>
2012-01-12 12:25:43 -0700

    Add a debug scheduler.
    This schedular serializes all the instances on a CPU.  Slow, of course,
    but easy to manage in a debugger.

---
+++ dax/cont/ScheduleDebug.h
@@ -0,0 +1,31 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __dax_cuda_cont_Schedule_h
+#define __dax_cuda_cont_Schedule_h
+
+#include <dax/Types.h>
+
+namespace dax {
+namespace cont {
+
+template<class Functor, class Parameters>
+DAX_CONT_EXPORT void scheduleDebug(Functor functor,
+                                   Parameters parameters,
+                                   dax::Id numInstances)
+{
+  for (dax::Id index = 0; index < numInstances; index++)
+    {
+    functor(parameters, index);
+    }
+}
+
+}
+} // namespace dax::cont
+
+#endif //__dax_cuda_cont_Schedule_h
--- dax/cont/UnitTestScheduleDebug.cxx
+++ dax/cont/UnitTestScheduleDebug.cxx
@@ -6,10 +6,7 @@
 
 =========================================================================*/
 
-#include <dax/cuda/cont/Schedule.h>
-
-#include <thrust/device_vector.h>
-#include <thrust/host_vector.h>
+#include <dax/cont/ScheduleDebug.h>
 
 #include <iostream>
 
@@ -21,7 +18,7 @@ const dax::Id OFFSET = 1000;
 
 }
 
-namespace ut_CudaSchedule {
+namespace {
 
 struct ClearArray
 {
@@ -39,28 +36,22 @@ struct AddArray
   }
 };
 
-} // namespace ut_CuadSchedule
+}
 
-int UnitTestCudaSchedule(int, char *[])
+int UnitTestScheduleDebug(int, char *[])
 {
-  using namespace ut_CudaSchedule;
-
-  thrust::device_vector<dax::Id> deviceArray(ARRAY_SIZE);
-  dax::Id *rawDeviceArray = thrust::raw_pointer_cast(&deviceArray[0]);
+  dax::Id array[ARRAY_SIZE];
 
   std::cout << "Running clear." << std::endl;
-  dax::cuda::cont::schedule(ClearArray(), rawDeviceArray, ARRAY_SIZE);
+  dax::cont::scheduleDebug(ClearArray(), array, ARRAY_SIZE);
 
   std::cout << "Running add." << std::endl;
-  dax::cuda::cont::schedule(AddArray(), rawDeviceArray, ARRAY_SIZE);
+  dax::cont::scheduleDebug(AddArray(), array, ARRAY_SIZE);
 
   std::cout << "Checking results." << std::endl;
-  thrust::host_vector<dax::Id> hostArray(ARRAY_SIZE);
-  thrust::copy(deviceArray.begin(), deviceArray.end(), hostArray.begin());
-
   for (dax::Id index = 0; index < ARRAY_SIZE; index++)
     {
-    dax::Id value = hostArray[index];
+    dax::Id value = array[index];
     if (value != index + OFFSET)
       {
       std::cout << "Got bad value." << std::endl;
