bc520c3bf0d3fb173fa6bca748bbd14bd480e077
Robert Maynard <robert.maynard@kitware.com>
2012-01-24 16:17:10 -0500

    Fixed numerous issues with the Cuda implementation of the unit test.

--- dax/cuda/cont/UnitTestCudaStreamCompact.cu
+++ dax/cuda/cont/UnitTestCudaStreamCompact.cu
@@ -6,13 +6,15 @@
 
 =========================================================================*/
 
-#include <dax/cont/ScheduleDebug.h>
-#include <dax/cont/StreamCompactDebug.h>
+#include <dax/cuda/cont/ScheduleThrust.h>
+#include <dax/cuda/cont/StreamCompact.h>
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+
+#include <thrust/device_vector.h>
 
 namespace {
 
@@ -37,9 +39,14 @@ static inline void test_assert_impl(bool condition,
     }
 }
 
+}
+
+namespace ut_CudaStreamCompact
+{
+
 struct InitArray
 {
-  DAX_EXEC_EXPORT void operator()(std::vector<dax::Id> &array, dax::Id index)
+  DAX_EXEC_EXPORT void operator()(dax::Id *array, dax::Id index)
   {
     array[index] = OFFSET + index;
   }
@@ -47,7 +54,7 @@ struct InitArray
 
 struct MarkOddNumbers
 {
-  DAX_EXEC_EXPORT void operator()(std::vector<dax::Id> &array, dax::Id index)
+  DAX_EXEC_EXPORT void operator()(dax::Id *array, dax::Id index)
   {
     array[index] = index%2;
   }
@@ -57,21 +64,24 @@ bool TestCompact()
   {
   //test the version of compact that takes in input and uses it as a stencil
   //and uses the index of each item as the value to place in the result vector
-  std::vector<dax::Id> array(ARRAY_SIZE);
-  std::vector<dax::Id> result;
+  ::thrust::device_vector<dax::Id> array(ARRAY_SIZE);
+  ::thrust::device_vector<dax::Id> result;
 
   array.resize(ARRAY_SIZE,dax::Id());
+  dax::Id *rawArray = thrust::raw_pointer_cast(&array[0]);
 
   //construct the index array
-  dax::cont::scheduleDebug(MarkOddNumbers(), array, ARRAY_SIZE);
-  dax::cont::streamCompactDebug(array,result);
+  dax::cuda::cont::scheduleThrust(MarkOddNumbers(),rawArray, ARRAY_SIZE);
+  dax::cuda::cont::streamCompact(array,result);
 
   test_assert(result.size() == array.size()/2,
               "result of compacation has an incorrect size.");
 
-  for (dax::Id index = 0; index < result.size(); index++)
+  ::thrust::host_vector<dax::Id> hresult(ARRAY_SIZE);
+  hresult = result;
+  for (dax::Id index = 0; index < static_cast<dax::Id>(hresult.size()); index++)
     {
-    dax::Id value = result[index];
+    dax::Id value = hresult[index];
     test_assert(value == (index*2+1),
                 "Incorrect value in compaction result.");
     }
@@ -81,24 +91,29 @@ bool TestCompact()
 bool TestCompactWithStencil()
   {
   //test the version of compact that takes in input and a stencil
-  std::vector<dax::Id> array(ARRAY_SIZE);
-  std::vector<dax::Id> stencil(ARRAY_SIZE);
-  std::vector<dax::Id> result;
+  ::thrust::device_vector<dax::Id> array(ARRAY_SIZE);
+  ::thrust::device_vector<dax::Id> stencil(ARRAY_SIZE);
+  ::thrust::device_vector<dax::Id> result;
 
   array.resize(ARRAY_SIZE,dax::Id());
   stencil.resize(ARRAY_SIZE,dax::Id());
 
+  dax::Id *rawArray = thrust::raw_pointer_cast(&array[0]);
+  dax::Id *rawStencil = thrust::raw_pointer_cast(&stencil[0]);
+
   //construct the index array
-  dax::cont::scheduleDebug(InitArray(), array, ARRAY_SIZE);
-  dax::cont::scheduleDebug(MarkOddNumbers(), stencil, ARRAY_SIZE);
-  dax::cont::streamCompactDebug(array,stencil,result);
+  dax::cuda::cont::scheduleThrust(InitArray(), rawArray, ARRAY_SIZE);
+  dax::cuda::cont::scheduleThrust(MarkOddNumbers(), rawStencil, ARRAY_SIZE);
+  dax::cuda::cont::streamCompact(array,stencil,result);
 
   test_assert(result.size() == array.size()/2,
               "result of compacation has an incorrect size.");
 
-  for (dax::Id index = 0; index < result.size(); index++)
+  ::thrust::host_vector<dax::Id> hresult(ARRAY_SIZE);
+  hresult = result;
+  for (dax::Id index = 0; index < static_cast<dax::Id>(hresult.size()); index++)
     {
-    dax::Id value = result[index];
+    dax::Id value = hresult[index];
     test_assert(value == (OFFSET + (index*2)+1),
                 "Incorrect value in compaction result.");
     }
@@ -112,7 +127,7 @@ int UnitTestCudaStreamCompact(int, char *[])
   bool valid = false;
   try
     {
-    valid = TestCompact();
+    valid = ut_CudaStreamCompact::TestCompact();
     }
   catch (std::string error)
     {
@@ -130,7 +145,7 @@ int UnitTestCudaStreamCompact(int, char *[])
   valid = false;
   try
     {
-    valid = TestCompactWithStencil();
+    valid = ut_CudaStreamCompact::TestCompactWithStencil();
     }
   catch (std::string error)
     {
