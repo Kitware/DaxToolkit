8a9f5054f1c454dddb794c00ec26af1e5b1cd1c7
Kenneth Moreland <kmorel@sandia.gov>
2012-01-23 17:02:01 -0700

    Add unit tests for raising errors in worklets.

---
+++ dax/cont/worklet/UnitTestWorkletCellMapError.cxx
@@ -0,0 +1,86 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include <dax/cont/worklet/testing/CellMapError.h>
+
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <dax/TypeTraits.h>
+
+#include <dax/cont/ErrorExecution.h>
+#include <dax/cont/UniformGrid.h>
+
+#include <vector>
+
+namespace {
+
+const dax::Id DIM = 64;
+
+#define test_assert(condition, message) \
+  test_assert_impl(condition, message, __FILE__, __LINE__);
+
+static inline void test_assert_impl(bool condition,
+                                    const std::string& message,
+                                    const char *file,
+                                    int line)
+{
+  if(!condition)
+    {
+    std::stringstream error;
+    error << file << ":" << line << std::endl;
+    error << message << std::endl;
+    throw error.str();
+    }
+}
+
+//-----------------------------------------------------------------------------
+static void TestCellMapError()
+{
+  dax::cont::UniformGrid grid;
+  grid.SetExtent(dax::make_Id3(0, 0, 0), dax::make_Id3(DIM-1, DIM-1, DIM-1));
+
+  std::cout << "Running field map worklet that errors" << std::endl;
+  bool gotError = false;
+  try
+    {
+    dax::cont::worklet::testing::CellMapError
+        <dax::cont::UniformGrid, dax::cont::DeviceAdapterDebug>(grid);
+    }
+  catch (dax::cont::ErrorExecution error)
+    {
+    std::cout << "Got expected ErrorExecution object." << std::endl;
+    test_assert(error.GetWorkletName() == "CellMapError",
+                "Got wrong worklet name.");
+    gotError = true;
+    }
+
+  test_assert(gotError, "Never got the error thrown.");
+}
+
+} // Anonymous namespace
+
+//-----------------------------------------------------------------------------
+int UnitTestWorkletCellMapError(int, char *[])
+{
+  try
+    {
+    TestCellMapError();
+    }
+  catch (std::string error)
+    {
+    std::cout
+        << "Encountered error: " << std::endl
+        << error << std::endl;
+    return 1;
+    }
+
+  return 0;
+}
---
+++ dax/cont/worklet/testing/CellMapError.h
@@ -0,0 +1,94 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __dax_cont_worklet_testing_CellMapError_h
+#define __dax_cont_worklet_testing_CellMapError_h
+
+// TODO: This should be auto-generated.
+
+#include <dax/Types.h>
+#include <dax/internal/DataArray.h>
+#include <dax/internal/GridStructures.h>
+#include <dax/exec/WorkMapField.h>
+#include <dax/exec/internal/ErrorHandler.h>
+#include <dax/exec/internal/FieldBuild.h>
+#include <dax/cont/ArrayHandle.h>
+#include <dax/cont/DeviceAdapter.h>
+#include <dax/cont/ErrorExecution.h>
+#include <dax/cont/internal/ExecutionPackageField.h>
+#include <dax/cont/internal/ExecutionPackageGrid.h>
+
+// TODO: Make generic math functions.
+#ifndef DAX_CUDA
+#include <math.h>
+#endif
+
+#include <Worklets/Testing/CellMapError.worklet>
+
+namespace dax {
+namespace exec {
+namespace kernel {
+
+template<class CellType>
+struct CellMapErrorParameters
+{
+  typename CellType::GridStructureType grid;
+};
+
+template<class CellType>
+struct CellMapError
+{
+  DAX_EXEC_EXPORT void operator()(
+      CellMapErrorParameters<CellType> &parameters,
+      dax::Id index,
+      const dax::exec::internal::ErrorHandler &errorHandler)
+  {
+    dax::exec::WorkMapCell<CellType> work(parameters.grid, errorHandler);
+    work.SetCellIndex(index);
+    dax::worklet::testing::CellMapError(work);
+  }
+};
+
+}
+}
+} // dax::exec::kernel
+
+namespace dax {
+namespace cont {
+namespace worklet {
+namespace testing {
+
+template<class GridType, class DeviceAdapter>
+inline void CellMapError(const GridType &grid)
+{
+  typedef dax::cont::internal::ExecutionPackageGrid<GridType> GridPackageType;
+  GridPackageType gridPackage(grid);
+
+  typedef typename GridPackageType::ExecutionCellType CellType;
+
+  typedef dax::exec::kernel::CellMapErrorParameters<CellType> Parameters;
+  Parameters parameters = {
+    gridPackage.GetExecutionObject()
+  };
+
+  char *error = DeviceAdapter::Schedule(
+        dax::exec::kernel::CellMapError<CellType>(),
+        parameters,
+        grid.GetNumberOfCells());
+
+  if ((error != NULL) && (error[0] != '\0'))
+    {
+    throw dax::cont::ErrorExecution(error, "CellMapError");
+    }
+}
+
+}
+}
+}
+} //dax::cont::worklet::testing
+
+#endif //__dax_cont_worklet_testing_CellMapError_h
