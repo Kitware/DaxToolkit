10dede6dc80713c17861d72b9041be1e067524ee
Robert Maynard <robert.maynard@kitware.com>
2012-01-27 12:35:18 -0500

    Update the unit test debug for stream compact.

--- dax/cont/UnitTestStreamCompactDebug.cxx
+++ dax/cont/UnitTestStreamCompactDebug.cxx
@@ -6,6 +6,7 @@
 
 =========================================================================*/
 
+#include <dax/cont/internal/ArrayContainerExecutionCPU.h>
 #include <dax/cont/ScheduleDebug.h>
 #include <dax/cont/StreamCompactDebug.h>
 
@@ -39,17 +40,19 @@ static inline void test_assert_impl(bool condition,
 
 struct InitArray
 {
-  DAX_EXEC_EXPORT void operator()(std::vector<dax::Id> &array, dax::Id index)
+  DAX_EXEC_EXPORT void operator()(dax::internal::DataArray<dax::Id>& array,
+                                  dax::Id index)
   {
-    array[index] = OFFSET + index;
+    array.SetValue(index,OFFSET + index);
   }
 };
 
 struct MarkOddNumbers
 {
-  DAX_EXEC_EXPORT void operator()(std::vector<dax::Id> &array, dax::Id index)
+  DAX_EXEC_EXPORT void operator()(dax::internal::DataArray<dax::Id>& array,
+                                  dax::Id index)
   {
-    array[index] = index%2;
+    array.SetValue(index,index%2);
   }
 };
 
@@ -57,22 +60,31 @@ bool TestCompact()
   {
   //test the version of compact that takes in input and uses it as a stencil
   //and uses the index of each item as the value to place in the result vector
-  std::vector<dax::Id> array(ARRAY_SIZE);
-  std::vector<dax::Id> result;
+  dax::cont::internal::ArrayContainerExecutionCPU<dax::Id> array;
+  dax::cont::internal::ArrayContainerExecutionCPU<dax::Id> result;
+  typedef dax::cont::internal::ArrayContainerExecutionCPU<dax::Id>::const_iterator
+      iterator;
 
-  array.resize(ARRAY_SIZE,dax::Id());
+  array.Allocate(ARRAY_SIZE);
+  dax::internal::DataArray<dax::Id> rawArray = array.GetExecutionArray();
 
   //construct the index array
-  dax::cont::scheduleDebug(MarkOddNumbers(), array, ARRAY_SIZE);
+  dax::cont::scheduleDebug(MarkOddNumbers(), rawArray, ARRAY_SIZE);
   dax::cont::streamCompactDebug(array,result);
 
-  test_assert(result.size() == array.size()/2,
-              "result of compacation has an incorrect size.");
+  std::stringstream buffer;
+  buffer << "result of compacation has an incorrect size of:";
+  buffer << result.GetNumberOfEntries();
+  test_assert(result.GetNumberOfEntries() == array.GetNumberOfEntries()/2,
+              buffer.str());
 
-  for (dax::Id index = 0; index < result.size(); index++)
+  dax::Id index=0;
+  for(iterator i = result.begin();
+      i != result.end();
+      ++i,++index)
     {
-    dax::Id value = result[index];
-    test_assert(value == (index*2+1),
+    const dax::Id value = *i;
+    test_assert(value == (index*2)+1,
                 "Incorrect value in compaction result.");
     }
   return true;
@@ -81,24 +93,35 @@ bool TestCompact()
 bool TestCompactWithStencil()
   {
   //test the version of compact that takes in input and a stencil
-  std::vector<dax::Id> array(ARRAY_SIZE);
-  std::vector<dax::Id> stencil(ARRAY_SIZE);
-  std::vector<dax::Id> result;
+  dax::cont::internal::ArrayContainerExecutionCPU<dax::Id> array;
+  dax::cont::internal::ArrayContainerExecutionCPU<dax::Id> stencil;
+  dax::cont::internal::ArrayContainerExecutionCPU<dax::Id> result;
+  typedef dax::cont::internal::ArrayContainerExecutionCPU<dax::Id>::const_iterator
+      iterator;
 
-  array.resize(ARRAY_SIZE,dax::Id());
-  stencil.resize(ARRAY_SIZE,dax::Id());
+  array.Allocate(ARRAY_SIZE);
+  stencil.Allocate(ARRAY_SIZE);
+
+  dax::internal::DataArray<dax::Id> rawArray = array.GetExecutionArray();
+  dax::internal::DataArray<dax::Id> rawStencil = stencil.GetExecutionArray();
 
   //construct the index array
-  dax::cont::scheduleDebug(InitArray(), array, ARRAY_SIZE);
-  dax::cont::scheduleDebug(MarkOddNumbers(), stencil, ARRAY_SIZE);
+  dax::cont::scheduleDebug(InitArray(), rawArray, ARRAY_SIZE);
+  dax::cont::scheduleDebug(MarkOddNumbers(), rawStencil, ARRAY_SIZE);
   dax::cont::streamCompactDebug(array,stencil,result);
 
-  test_assert(result.size() == array.size()/2,
-              "result of compacation has an incorrect size.");
+  std::stringstream buffer;
+  buffer << "result of compacation has an incorrect size of:";
+  buffer << result.GetNumberOfEntries();
+  test_assert(result.GetNumberOfEntries() == array.GetNumberOfEntries()/2,
+              buffer.str());
 
-  for (dax::Id index = 0; index < result.size(); index++)
+  dax::Id index=0;
+  for(iterator i = result.begin();
+      i != result.end();
+      ++i,++index)
     {
-    dax::Id value = result[index];
+    const dax::Id value = *i;
     test_assert(value == (OFFSET + (index*2)+1),
                 "Incorrect value in compaction result.");
     }
