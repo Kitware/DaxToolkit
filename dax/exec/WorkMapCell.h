0c0620c0fb5dc1d559e3841bdac14a82d2d9f8d3
Robert Maynard <robert.maynard@kitware.com>
2012-02-02 13:57:12 -0500

    Added in a specilization for WorkMapCellhexahedron.

--- dax/exec/WorkMapCell.h
+++ dax/exec/WorkMapCell.h
@@ -88,6 +88,72 @@ public:
   }
 };
 
+///----------------------------------------------------------------------------
+// Work for worklets that map points to cell. Use this work when the worklets
+// need "CellArray" information i.e. information about what points form a cell.
+template<>
+class WorkMapCell<dax::exec::CellHexahedron>
+{
+private:
+  dax::exec::CellHexahedron Cell;
+
+public:
+  typedef CellHexahedron CellType;
+
+  DAX_EXEC_EXPORT WorkMapCell(
+    const dax::internal::UnstructuredGrid<CellType> &gridStructure,
+    dax::Id cellIndex = 0)
+    : Cell(gridStructure, cellIndex) { }
+
+  DAX_EXEC_EXPORT const dax::exec::CellHexahedron GetCell() const
+  {
+    return this->Cell;
+  }
+
+  template<typename T>
+  DAX_EXEC_EXPORT
+  const T &GetFieldValue(const dax::exec::FieldCell<T> &field) const
+  {
+    return dax::exec::internal::fieldAccessNormalGet(field,
+                                                     this->GetCellIndex());
+  }
+
+  template<typename T>
+  DAX_EXEC_EXPORT void SetFieldValue(dax::exec::FieldCell<T> &field,
+                                     const T &value)
+  {
+    dax::exec::internal::fieldAccessNormalSet(field,
+                                              this->GetCellIndex(),
+                                              value);
+  }
+
+  template<typename T>
+  DAX_EXEC_EXPORT const T &GetFieldValue(const dax::exec::FieldPoint<T> &field,
+                                         dax::Id vertexIndex) const
+  {
+    dax::Id pointIndex = this->GetCell().GetPointIndex(vertexIndex);
+    return dax::exec::internal::fieldAccessNormalGet(field, pointIndex);
+  }
+
+  DAX_EXEC_EXPORT dax::Vector3 GetFieldValue(
+    const dax::exec::FieldCoordinates &, dax::Id vertexIndex) const
+  {
+    dax::Id pointIndex = this->GetCell().GetPointIndex(vertexIndex);
+    const dax::internal::UnstructuredGrid<CellType> &gridStructure
+        = this->GetCell().GetGridStructure();
+    return
+        dax::exec::internal::fieldAccessUniformCoordinatesGet(gridStructure,
+                                                              pointIndex);
+  }
+
+  DAX_EXEC_EXPORT dax::Id GetCellIndex() const { return this->Cell.GetIndex(); }
+
+  DAX_EXEC_EXPORT void SetCellIndex(dax::Id cellIndex)
+  {
+    this->Cell.SetIndex(cellIndex);
+  }
+};
+
 
 }
 }
