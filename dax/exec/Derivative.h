8d7fef9570df02887104bbf60376c4c2efa6cc0a
Robert Maynard <robert.maynard@kitware.com>
2012-02-13 15:36:22 -0500

    Add basic support to finding the hexahedron derivative.
    Really only supports hexahedrons that are voxels.

--- dax/exec/Derivative.h
+++ dax/exec/Derivative.h
@@ -40,6 +40,45 @@ DAX_EXEC_EXPORT dax::Vector3 cellDerivative(
   return sum/cell.GetSpacing();
 }
 
+//-----------------------------------------------------------------------------
+template<class WorkType>
+DAX_EXEC_EXPORT dax::Vector3 cellDerivative(
+    const WorkType &work,
+    const dax::exec::CellHexahedron &,
+    const dax::Vector3 &pcoords,
+    const dax::exec::FieldCoordinates &fcoords,
+    const dax::exec::FieldPoint<dax::Scalar> &point_scalar)
+{
+  //for know we are considering that a cell hexahedron
+  //is actually a voxel in an unstructured grid.
+  //ToDo: use a proper derivative calculation.
+
+  const dax::Id numVerts = 8;
+  dax::Vector3 derivativeWeights[numVerts];
+  dax::exec::internal::derivativeWeightsVoxel(pcoords, derivativeWeights);
+
+
+  dax::Vector3 spacing;
+    {
+    dax::Vector3 x0 = work.GetFieldValue(fcoords,0);
+    dax::Vector3 x1 = work.GetFieldValue(fcoords,1);
+    dax::Vector3 x2 = work.GetFieldValue(fcoords,2);
+    dax::Vector3 x4 = work.GetFieldValue(fcoords,4);
+    spacing = make_Vector3(x1[0] - x0[0],
+                           x2[1] - x0[1],
+                           x4[2] - x0[2]);
+    }
+
+  dax::Vector3 sum = dax::make_Vector3(0.0, 0.0, 0.0);
+  for (dax::Id vertexId = 0; vertexId < numVerts; vertexId++)
+    {
+    dax::Scalar value = work.GetFieldValue(point_scalar, vertexId);
+    sum = sum + value * derivativeWeights[vertexId];
+    }
+
+  return sum/spacing;
+}
+
 }};
 
 #endif //__dax_exec_Derivative_h
