7526eda2034ec35fc2a9c6dd4f37817932c1a7cc
Robert Maynard <robert.maynard@kitware.com>
2012-02-17 13:31:46 -0500

    Updated the device adapter to use the new test grid helper.

--- dax/cont/internal/TestingDeviceAdapter.h
+++ dax/cont/internal/TestingDeviceAdapter.h
@@ -21,6 +21,7 @@
 
 #include <dax/cont/internal/IteratorContainer.h>
 #include <dax/cont/internal/Testing.h>
+#include <dax/cont/internal/TestingGridGenerator.h>
 
 #include <dax/internal/DataArray.h>
 
@@ -85,69 +86,6 @@ public:
   };
 
 private:
-
-  //helper structs needed to have worklet tests work
-  //on both uniform and unstructured grids
-  struct MakeGrid
-  {
-    MakeGrid(dax::cont::UniformGrid &grid)
-      {
-      grid.SetExtent(dax::make_Id3(0, 0, 0), dax::make_Id3(DIM-1, DIM-1, DIM-1));
-      }
-
-    MakeGrid(dax::cont::UnstructuredGrid<dax::exec::CellHexahedron> &grid)
-      {
-      //we need to make a volume grid
-      dax::cont::UniformGrid uniform;
-      uniform.SetExtent(dax::make_Id3(0, 0, 0), dax::make_Id3(DIM-1, DIM-1, DIM-1));
-
-      //copy the point info over to the unstructured grid
-      points.clear();
-      for(dax::Id i=0; i <uniform.GetNumberOfPoints(); ++i)
-        {
-        points.push_back(uniform.GetPointCoordinates(i));
-        }
-
-      //copy the cell topology information over
-
-      //this only works for voxel/hexahedron
-      const dax::Id3 cellVertexToPointIndex[8] = {
-        dax::make_Id3(0, 0, 0),
-        dax::make_Id3(1, 0, 0),
-        dax::make_Id3(1, 1, 0),
-        dax::make_Id3(0, 1, 0),
-        dax::make_Id3(0, 0, 1),
-        dax::make_Id3(1, 0, 1),
-        dax::make_Id3(1, 1, 1),
-        dax::make_Id3(0, 1, 1)
-      };
-
-      topology.clear();
-      dax::Id numPointsPerCell = dax::exec::CellHexahedron::NUM_POINTS;
-      const dax::Extent3 extents = uniform.GetExtent();
-      for(dax::Id i=0; i <uniform.GetNumberOfCells(); ++i)
-        {
-        dax::Id3 ijkCell = dax::flatIndexToIndex3Cell(i,extents);
-        for(dax::Id j=0; j < numPointsPerCell; ++j)
-          {
-          dax::Id3 ijkPoint = ijkCell + cellVertexToPointIndex[j];
-
-          dax::Id pointIndex = index3ToFlatIndex(ijkPoint,extents);
-          topology.push_back(pointIndex);
-          }
-        }
-
-      dax::cont::ArrayHandle<dax::Vector3> ahPoints(points.begin(),points.end());
-      dax::cont::ArrayHandle<dax::Id> ahTopo(topology.begin(),topology.end());
-
-      grid.UpdateHandles(ahTopo,ahPoints);
-      }
-
-    std::vector<dax::Id> topology;
-    std::vector<dax::Vector3> points;
-  };
-
-
 
   // Note: this test does not actually test to make sure the data is available
   // in the execution environment. It tests to make sure data gets to the array
@@ -286,34 +224,34 @@ private:
     std::cout << "-------------------------------------------" << std::endl;
     std::cout << "Testing basic map field worklet" << std::endl;
 
-    GridType grid;
-    MakeGrid mg(grid);(void)mg; //need mg around to use storage for unstructured grid :hack:
-
+    //use a scoped pointer that constructs and fills a grid of the
+    //right type
+    dax::cont::internal::TestGrid<GridType,DeviceAdapter> grid(DIM);
 
     dax::Vector3 trueGradient = dax::make_Vector3(1.0, 1.0, 1.0);
 
-    std::vector<dax::Scalar> field(grid.GetNumberOfPoints());
-    std::cout << "Number of Points in the grid" <<  grid.GetNumberOfPoints() << std::endl;
+    std::vector<dax::Scalar> field(grid->GetNumberOfPoints());
+    std::cout << "Number of Points in the grid" <<  grid->GetNumberOfPoints() << std::endl;
     for (dax::Id pointIndex = 0;
-         pointIndex < grid.GetNumberOfPoints();
+         pointIndex < grid->GetNumberOfPoints();
          pointIndex++)
       {
       field[pointIndex]
-          = dax::dot(grid.GetPointCoordinates(pointIndex), trueGradient);
+          = dax::dot(grid->GetPointCoordinates(pointIndex), trueGradient);
       }
     dax::cont::ArrayHandle<dax::Scalar, DeviceAdapter>
         fieldHandle(field.begin(), field.end());
 
-    std::vector<dax::Scalar> square(grid.GetNumberOfPoints());
+    std::vector<dax::Scalar> square(grid->GetNumberOfPoints());
     dax::cont::ArrayHandle<dax::Scalar, DeviceAdapter>
         squareHandle(square.begin(), square.end());
 
     std::cout << "Running Square worklet" << std::endl;
-    dax::cont::worklet::Square(grid, fieldHandle, squareHandle);
+    dax::cont::worklet::Square(grid.GetRealGrid(), fieldHandle, squareHandle);
 
     std::cout << "Checking result" << std::endl;
     for (dax::Id pointIndex = 0;
-         pointIndex < grid.GetNumberOfPoints();
+         pointIndex < grid->GetNumberOfPoints();
          pointIndex++)
       {
       dax::Scalar squareValue = square[pointIndex];
@@ -329,15 +267,14 @@ private:
     std::cout << "-------------------------------------------" << std::endl;
     std::cout << "Testing map field worklet error" << std::endl;
 
-    GridType grid;
-    MakeGrid mg(grid);(void)mg; //need mg around to use storage for unstructured grid :hack:
+    dax::cont::internal::TestGrid<GridType,DeviceAdapter> grid(DIM);
 
     std::cout << "Running field map worklet that errors" << std::endl;
     bool gotError = false;
     try
       {
       dax::cont::worklet::testing::FieldMapError
-          <GridType, DeviceAdapter>(grid);
+          <GridType, DeviceAdapter>(grid.GetRealGrid());
       }
     catch (dax::cont::ErrorExecution error)
       {
@@ -355,35 +292,34 @@ private:
     std::cout << "-------------------------------------------" << std::endl;
     std::cout << "Testing basic map cell worklet" << std::endl;
 
-    GridType grid;
-    MakeGrid mg(grid);(void)mg; //need mg around to use storage for unstructured grid :hack:
+    dax::cont::internal::TestGrid<GridType,DeviceAdapter> grid(DIM);
 
     dax::Vector3 trueGradient = dax::make_Vector3(1.0, 1.0, 1.0);
 
-    std::vector<dax::Scalar> field(grid.GetNumberOfPoints());
+    std::vector<dax::Scalar> field(grid->GetNumberOfPoints());
     for (dax::Id pointIndex = 0;
-         pointIndex < grid.GetNumberOfPoints();
+         pointIndex < grid->GetNumberOfPoints();
          pointIndex++)
       {
       field[pointIndex]
-          = dax::dot(grid.GetPointCoordinates(pointIndex), trueGradient);
+          = dax::dot(grid->GetPointCoordinates(pointIndex), trueGradient);
       }
     dax::cont::ArrayHandle<dax::Scalar, DeviceAdapter>
         fieldHandle(field.begin(), field.end());
 
-    std::vector<dax::Vector3> gradient(grid.GetNumberOfCells());
+    std::vector<dax::Vector3> gradient(grid->GetNumberOfCells());
     dax::cont::ArrayHandle<dax::Vector3, DeviceAdapter>
         gradientHandle(gradient.begin(), gradient.end());
 
     std::cout << "Running CellGradient worklet" << std::endl;
-    dax::cont::worklet::CellGradient(grid,
-                                     grid.GetPoints(),
+    dax::cont::worklet::CellGradient(grid.GetRealGrid(),
+                                     grid->GetPoints(),
                                      fieldHandle,
                                      gradientHandle);
 
     std::cout << "Checking result" << std::endl;
     for (dax::Id cellIndex = 0;
-         cellIndex < grid.GetNumberOfCells();
+         cellIndex < grid->GetNumberOfCells();
          cellIndex++)
       {
       dax::Vector3 gradientValue = gradient[cellIndex];
@@ -398,15 +334,14 @@ private:
     std::cout << "-------------------------------------------" << std::endl;
     std::cout << "Testing map cell worklet error" << std::endl;
 
-    GridType grid;
-    MakeGrid mg(grid);(void)mg; //need mg around to use storage for unstructured grid :hack:
+    dax::cont::internal::TestGrid<GridType,DeviceAdapter> grid(DIM);
 
     std::cout << "Running cell map worklet that errors" << std::endl;
     bool gotError = false;
     try
       {
       dax::cont::worklet::testing::CellMapError
-          <GridType, DeviceAdapter>(grid);
+          <GridType, DeviceAdapter>(grid.GetRealGrid());
       }
     catch (dax::cont::ErrorExecution error)
       {
