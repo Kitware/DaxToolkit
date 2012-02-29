302c49f7a79e0e1eaad86bf511b87a151b48ee56
Robert Maynard <robert.maynard@kitware.com>
2012-02-29 14:56:50 -0500

    No needed for a specialization for MapField either.

--- dax/exec/WorkMapField.h
+++ dax/exec/WorkMapField.h
@@ -30,47 +30,20 @@ class WorkMapField
 
 public:
   typedef CellT CellType;
+  typedef typename CellType::TopologyType TopologyType;
+
+  const TopologyType GridTopology;
+
+  DAX_EXEC_CONT_EXPORT WorkMapField(
+      const TopologyType &gs,
+      const dax::exec::internal::ErrorHandler &errorHandler)
+    : ErrorHandler(errorHandler),
+      GridTopology(gs){ }
 
   DAX_EXEC_CONT_EXPORT WorkMapField(
       const dax::exec::internal::ErrorHandler &errorHandler)
-    : ErrorHandler(errorHandler) { }
-
-  template<typename T>
-  DAX_EXEC_EXPORT const T &GetFieldValue(const dax::exec::Field<T> &field) const
-  {
-    return dax::exec::internal::fieldAccessNormalGet(field, this->GetIndex());
-  }
-
-  template<typename T>
-  DAX_EXEC_EXPORT void SetFieldValue(dax::exec::Field<T> &field, const T &value)
-  {
-    dax::exec::internal::fieldAccessNormalSet(field, this->GetIndex(), value);
-  }
-
-  DAX_EXEC_EXPORT dax::Id GetIndex() const { return this->Index; }
-
-  DAX_EXEC_EXPORT void SetIndex(dax::Id index) { this->Index = index; }
-
-  DAX_EXEC_EXPORT void RaiseError(const char *message)
-  {
-    this->ErrorHandler.RaiseError(message);
-  }
-};
-
-template<>
-class WorkMapField<dax::exec::CellVoxel>
-{
-  const dax::internal::TopologyUniform GridTopology;
-  dax::Id Index;
-  dax::exec::internal::ErrorHandler ErrorHandler;
-
-public:
-  typedef CellVoxel CellType;
-
-  DAX_EXEC_CONT_EXPORT WorkMapField(
-      const dax::internal::TopologyUniform &gs,
-      const dax::exec::internal::ErrorHandler &errorHandler)
-    : GridTopology(gs), ErrorHandler(errorHandler) { }
+    : ErrorHandler(errorHandler),
+      GridTopology(){ }
 
   template<typename T>
   DAX_EXEC_EXPORT const T &GetFieldValue(const dax::exec::Field<T> &field) const
@@ -102,58 +75,6 @@ public:
     this->ErrorHandler.RaiseError(message);
   }
 };
-
-
-
-template<>
-class WorkMapField<dax::exec::CellHexahedron>
-{
-public:
-  typedef CellHexahedron CellType;
-
-protected:
-  const dax::internal::TopologyUnstructured<CellType> GridTopology;
-  dax::Id Index;
-  dax::exec::internal::ErrorHandler ErrorHandler;
-
-public:
-
-  DAX_EXEC_CONT_EXPORT WorkMapField(
-      const dax::internal::TopologyUnstructured<CellType> &gs,
-      const dax::exec::internal::ErrorHandler &errorHandler)
-    : GridTopology(gs), ErrorHandler(errorHandler) { }
-
-  template<typename T>
-  DAX_EXEC_EXPORT const T &GetFieldValue(const dax::exec::Field<T> &field) const
-  {
-    return dax::exec::internal::fieldAccessNormalGet(field, this->GetIndex());
-  }
-
-  template<typename T>
-  DAX_EXEC_EXPORT void SetFieldValue(dax::exec::Field<T> &field, const T &value)
-  {
-    dax::exec::internal::fieldAccessNormalSet(field, this->GetIndex(), value);
-  }
-
-  DAX_EXEC_EXPORT dax::Vector3 GetFieldValue(
-    const dax::exec::FieldCoordinates &)
-  {
-    // Special case.  Point coordiantes are determined implicitly by index.
-    return dax::exec::internal::fieldAccessUniformCoordinatesGet(
-          this->GridTopology,
-          this->GetIndex());
-  }
-
-  DAX_EXEC_EXPORT dax::Id GetIndex() const { return this->Index; }
-
-  DAX_EXEC_EXPORT void SetIndex(dax::Id index) { this->Index = index; }
-
-  DAX_EXEC_EXPORT void RaiseError(const char *message)
-  {
-    this->ErrorHandler.RaiseError(message);
-  }
-};
-
 
 }}
 
