c1d3dd36b8ad7a7ff20f33d14b9f62ceb03e9d7e
Robert Maynard <robert.maynard@kitware.com>
2012-03-09 15:23:37 -0500

    Implement debug version of unique.

--- dax/cont/UniqueDebug.h
+++ dax/cont/UniqueDebug.h
@@ -11,13 +11,20 @@
 
 #include <dax/cont/internal/ArrayContainerExecutionCPU.h>
 
+#include <algorithm>
+
 namespace dax {
 namespace cont {
 
 
 template<typename T>
-DAX_CONT_EXPORT void uniqueDebug(T &values)
+DAX_CONT_EXPORT void uniqueDebug(dax::cont::internal::ArrayContainerExecutionCPU<T> &values)
 {
+  typedef typename dax::cont::internal::ArrayContainerExecutionCPU<T>::iterator resultType;
+
+  resultType newEnd = std::unique(values.begin(),values.end());
+  values.Allocate( std::distance(values.begin(),newEnd) );
+
 }
 
 }
