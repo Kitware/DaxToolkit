688c8b9d8e585fe659ac16cd7dd65093f9da42e1
Robert Maynard <robert.maynard@kitware.com>
2012-03-13 10:00:23 -0400

    Add in optional code to dump the output of threshold to file.

--- Testing/Threshold/Pipeline.h
+++ Testing/Threshold/Pipeline.h
@@ -18,6 +18,8 @@
 #include <dax/cont/worklet/Threshold.h>
 
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 #define MAKE_STRING2(x) #x
 #define MAKE_STRING1(x) MAKE_STRING2(x)
@@ -71,6 +73,67 @@ void PrintResults(int pipeline, double time)
             << pipeline << "," << time << std::endl;
 }
 
+template<typename T, typename Stream>
+void PrintContentsToStream(dax::cont::UnstructuredGrid<T>& grid, Stream &stream)
+  {
+  const dax::Id num_points(grid.GetNumberOfPoints());
+  const dax::Id num_cells(grid.GetNumberOfCells());
+
+  //dump header
+  stream << "# vtk DataFile Version 3.0"  << std::endl;
+  stream << "vtk output" << std::endl;
+  stream << "ASCII"  << std::endl;
+  stream << "DATASET UNSTRUCTURED_GRID" << std::endl;
+  stream << "POINTS " << num_points << " float" << std::endl;
+
+  std::vector<dax::Vector3> contPoints(num_points);
+  grid.GetCoordinatesHandle().SetNewControlData(contPoints.begin(),contPoints.end());
+  grid.GetCoordinatesHandle().CompleteAsOutput();
+
+  for(dax::Id i=0; i < num_points; ++i)
+    {
+    dax::Vector3 coord = contPoints[i];
+    stream << coord[0] << " " << coord[1] << " " << coord[2] << " ";
+    if(i%3==2)
+      {
+      stream << std::endl; //pump new line after each 3rd vector
+      }
+    }
+  if(num_points%3==2)
+    {
+    stream << std::endl;
+    }
+
+  //print cells
+  stream << "CELLS " << num_cells << " " << num_cells  * (T::NUM_POINTS+1) << std::endl;
+
+  std::vector<dax::Id> contTopo(num_cells*T::NUM_POINTS);
+  grid.GetTopologyHandle().SetNewControlData(contTopo.begin(),contTopo.end());
+  grid.GetTopologyHandle().CompleteAsOutput();
+
+  dax::Id index=0;
+  for(dax::Id i=0; i < num_cells; ++i,index+=8)
+    {
+    stream << T::NUM_POINTS << " ";
+    stream << contTopo[index+0] << " ";
+    stream << contTopo[index+1] << " ";
+    stream << contTopo[index+3] << " ";
+    stream << contTopo[index+2] << " ";
+    stream << contTopo[index+4] << " ";
+    stream << contTopo[index+5] << " ";
+    stream << contTopo[index+7] << " ";
+    stream << contTopo[index+6] << " ";
+
+    stream << std::endl;
+    }
+  stream << std::endl;
+  stream << "CELL_TYPES " << num_cells << std::endl;
+  for(dax::Id i=0; i < num_cells; ++i)
+    {
+    stream << "11" << std::endl; //11 is voxel && 12 is hexa
+    }
+  }
+
 void RunDAXPipeline(const dax::cont::UniformGrid &grid)
 {
   std::cout << "Running pipeline 1: Elevation -> Threshold" << std::endl;
@@ -90,11 +153,22 @@ void RunDAXPipeline(const dax::cont::UniformGrid &grid)
   double time = timer.elapsed();
   std::cout << "original GetNumberOfCells: " << grid.GetNumberOfCells() << std::endl;
   std::cout << "threshold GetNumberOfCells: " << grid2.GetNumberOfCells() << std::endl;
+
+  std::cout << "original GetNumberOfPoints: " << grid.GetNumberOfPoints() << std::endl;
+  std::cout << "threshold GetNumberOfPoints: " << grid2.GetNumberOfPoints() << std::endl;
   PrintResults(1, time);
 
   std::vector<dax::Scalar> resultsBuffer(resultHandle.GetNumberOfEntries());
   resultHandle.SetNewControlData(resultsBuffer.begin(),resultsBuffer.end());
   resultHandle.CompleteAsOutput(); //fetch back to control
+
+
+
+  //rough dump to file
+  //std::ofstream file;
+  //file.open ("daxResult.vtk");
+  //PrintContentsToStream(grid2,file);
+  //file.close();
 
   CheckValues(resultsBuffer.begin(), resultsBuffer.end());
 }
