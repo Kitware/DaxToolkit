f12b466069312bb3c2ed0a649dd01d725e9825a0
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2012-08-09 11:32:40 -0400

    Working logic for topology generation

--- dax/cont/worklet/MarchingCubes.h
+++ dax/cont/worklet/MarchingCubes.h
@@ -92,19 +92,35 @@ private:
 };
 
 // ------------------------------------------------------------------- Generate
-template<class InputTopologyType, class OutputTopologyType>
+template<class InputTopologyType,
+         class ValuesPortalType,
+         class CountPortalType,
+         class OutputPortalType,
+         class OutputTopologyType>
 struct MarchingCubesGenerateTopologyFunctor
+
 {
   // ................................................................ typesdefs
   typedef typename InputTopologyType::CellType  InputCellType;
+  typedef typename ValuesPortalType::ValueType  ValueType;
+  typedef typename CountPortalType::ValueType   CountType;
+  typedef typename OutputPortalType::ValueType  OutputType;
   typedef typename OutputTopologyType::CellType OutputCellType;
 
-  DAX_CONT_EXPORT MarchingCubesGenerateTopologyFunctor(
-      const dax::worklet::MarchingCubesTopology &worklet,
-      const InputTopologyType &inputTopology,
-      const OutputTopologyType &outputTopology)
+  // ..................................................................... cnstr
+  DAX_CONT_EXPORT
+  MarchingCubesGenerateTopologyFunctor(
+      const dax::worklet::MarchingCubesTopology<ValueType> &worklet,
+      const InputTopologyType                              &inputTopology,
+      const ValuesPortalType                               &values,
+      const CountPortalType                                &count,
+      const OutputPortalType                               &output,
+      const OutputTopologyType                             &outputTopology)
     : Worklet(worklet),
       InputTopology(inputTopology),
+      Values(values),
+      Count(count),
+      Output(output),
       OutputTopology(outputTopology) {  }
 
   // ....................................................................... ()
@@ -115,32 +131,45 @@ struct MarchingCubesGenerateTopologyFunctor
       const dax::exec::internal::ErrorMessageBuffer &errorMessage)
   {
     this->Worklet.SetErrorMessageBuffer(errorMessage);
-    const dax::worklet::MarchingCubesTopology &constWorklet = this->Worklet;
+    const dax::worklet::MarchingCubesTopology<ValueType> &constWorklet = this->Worklet;
 
     InputCellType inputCell(this->InputTopology, inputCellIndex);
-    typename OutputCellType::PointConnectionsType outputCellConnections;
+    dax::Tuple<dax::Vector3,3*5> outputPoints;
 
-    constWorklet(inputCell, outputCellConnections);
+    constWorklet(inputCell,
+                 dax::exec::internal::FieldGetPointsForCell(this->Values,
+                                                            inputCell,
+                                                            constWorklet),
+                 outputPoints);
 
-    // Write cell connections back to cell array.
-    dax::Id index = outputCellIndex * OutputCellType::NUM_POINTS;
     for (int localIndex = 0;
-         localIndex < OutputCellType::NUM_POINTS;
+         localIndex < this->Count.Get(inputCellIndex)*OutputCellType::NUM_POINTS;
          localIndex++)
       {
       // This only actually works if OutputTopologyType is TopologyUnstructured.
+      for(int i =0 ;i<3;++i)
+        {
+        // This only actually works if OutputTopologyType is TopologyUnstructured.
+        dax::exec::internal::FieldSet(this->Output,
+                                      outputCellIndex*OutputCellType::NUM_POINTS+localIndex +i,
+                                      outputPoints[localIndex][i],
+                                      constWorklet);
+        }
       dax::exec::internal::FieldSet(this->OutputTopology.CellConnections,
-                                    index,
-                                    outputCellConnections[localIndex],
-                                    constWorklet);
-      index++;
+                                   OutputCellType::NUM_POINTS*outputCellIndex+localIndex,
+                                   OutputCellType::NUM_POINTS*outputCellIndex+localIndex,
+                                   constWorklet);
       }
   }
 
 private:
-  dax::worklet::MarchingCubesTopology Worklet;
-  InputTopologyType InputTopology;
-  OutputTopologyType OutputTopology;
+  // ..................................................................... vars
+  dax::worklet::MarchingCubesTopology<ValueType> Worklet;
+  InputTopologyType                                               InputTopology;
+  ValuesPortalType                                                Values;
+  CountPortalType                                                 Count;
+  OutputPortalType                                                Output;
+  OutputTopologyType                                              OutputTopology;
 };
 
 // -------------------------------------------------------------- MarchingCubes
@@ -154,10 +183,10 @@ class MarchingCubes : public dax::cont::internal::ScheduleGenerateMangledTopolog
     Adapter>
 {
 protected:
+  // ................................................................ typesdefs
   typedef dax::cont::internal::ScheduleGenerateMangledTopology<
       MarchingCubes<ValueType,Container1,Container2,Adapter>, Adapter> Superclass;
   typedef typename Superclass::ArrayHandleId ArrayHandleId;
-  typedef typename Superclass::ArrayHandleMask ArrayHandleMask;
 
 public:
   // .................................................................... cnstr
@@ -202,42 +231,58 @@ public:
   // .................................................... CreateTopologyFunctor
   //generate the functor for the topology generation worklet
   template<class InputGridType, class OutputGridType>
-  MarchingCubesGenerateTopologyFunctor<
-      typename InputGridType::TopologyStructConstExecution,
-      typename OutputGridType::TopologyStructExecution>
+  MarchingCubesGenerateTopologyFunctor
+  <
+    typename InputGridType::TopologyStructConstExecution,
+    typename dax::cont::ArrayHandle<ValueType,
+                                    Container1,
+                                    Adapter>::PortalConstExecution,
+    typename ArrayHandleId::PortalConstExecution,
+    typename dax::cont::ArrayHandle<ValueType,
+                                    Container2,
+                                    Adapter>::PortalExecution,
+    typename OutputGridType::TopologyStructExecution
+  >
   CreateTopologyFunctor(const InputGridType &inputGrid,
                         OutputGridType &outputGrid,
-                        dax::Id outputGridSize)
+                        dax::Id outputGridSize,
+                        const ArrayHandleId &reducedCellCount)
   {
     typedef MarchingCubesGenerateTopologyFunctor<
         typename InputGridType::TopologyStructConstExecution,
-        typename OutputGridType::TopologyStructExecution> FunctorType;
+        typename dax::cont::ArrayHandle<ValueType,
+                                        Container1,
+                                        Adapter>::PortalConstExecution,
+        typename ArrayHandleId::PortalConstExecution,
+        typename dax::cont::ArrayHandle<ValueType,
+                                        Container2,
+                                        Adapter>::PortalExecution,
+        typename OutputGridType::TopologyStructExecution>
+      FunctorType;
 
-    dax::worklet::MarchingCubesTopology worklet;
+    typedef typename dax::cont::ArrayHandle<ValueType,
+                                            Container2,
+                                            Adapter>::PortalExecution
+      OutputPortal;
+
+    dax::worklet::MarchingCubesTopology<ValueType> worklet(this->IsoValue);
 
     FunctorType functor(
           worklet,
           inputGrid.PrepareForInput(),
-          outputGrid.PrepareForOutput(outputGridSize));
+          this->InputHandle.PrepareForInput(),
+          reducedCellCount.PrepareForInput(),
+          this->OutputHandle.PrepareForOutput(outputGridSize*3*3),
+          outputGrid.PrepareForOutput(outputGridSize*3));
 
     return functor;
   }
 
-  //MarchingCubes any fields that are needed
-  void GenerateOutputFields(const ArrayHandleMask &pointMask)
-  {
-    //we know that the MarchingCubes is being done on a point field
-    dax::cont::internal::StreamCompact(this->InputHandle,
-                                       pointMask,
-                                       this->OutputHandle,
-                                       Adapter());
-  }
-
 private:
-  ValueType IsoValue;
-
+  // ..................................................................... vars
+  ValueType                                                         IsoValue;
   const dax::cont::ArrayHandle<ValueType,Container1,Adapter> &InputHandle;
-  dax::cont::ArrayHandle<ValueType,Container2,Adapter> &OutputHandle;
+  dax::cont::ArrayHandle<ValueType,Container2,Adapter>      &OutputHandle;
 };
 
 }
@@ -250,12 +295,12 @@ namespace dax {
 namespace cont {
 namespace worklet {
 
+// ------------------------------------------------------------ MarchingCubes()
 template<class InGridType,
          class OutGridType,
          typename ValueType,
          class Container1,
          class Container2,
-// ------------------------------------------------------------ MarchingCubes()
          class Adapter>
 inline void MarchingCubes(
     const InGridType &inGrid,
