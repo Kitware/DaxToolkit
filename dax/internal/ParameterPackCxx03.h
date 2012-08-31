9efed77632a7d6d24543ae63b1c120741f57399d
Brad King <brad.king@kitware.com>
2012-08-31 09:50:50 -0400

    Add dax::internal::Members class template
    Add a class template that offers storage of arbitrary "members" that can
    be indexed programmatically by compile-time constructs.

--- dax/internal/ParameterPackCxx03.h
+++ dax/internal/ParameterPackCxx03.h
@@ -26,11 +26,21 @@
 #  include <boost/preprocessor/arithmetic/dec.hpp>
 #  include <boost/preprocessor/iteration/iterate.hpp>
 #  include <boost/preprocessor/punctuation/comma_if.hpp>
+#  include <boost/preprocessor/repetition/enum_shifted.hpp>
+#  include <boost/preprocessor/repetition/enum_shifted_binary_params.hpp>
 #  include <boost/preprocessor/repetition/enum_shifted_params.hpp>
+#  include <boost/preprocessor/repetition/repeat_from_to.hpp>
 #  define _dax_pp_T___            BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_ITERATION(), T___)
 #  define _dax_pp_typename___T    BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_ITERATION(), typename T___)
 #  define _dax_pp_sizeof___T      BOOST_PP_DEC(BOOST_PP_ITERATION())
 #  define _dax_pp_comma           BOOST_PP_COMMA_IF(_dax_pp_sizeof___T)
+#  define _dax_pp_enum___(x)      BOOST_PP_ENUM_SHIFTED(BOOST_PP_ITERATION(), _dax_pp_enum, x)
+#  define _dax_pp_enum(z,n,x)     _dax_pp_enum_(z,n,x)
+#  define _dax_pp_enum_(z,n,x)    x(n)
+#  define _dax_pp_repeat___(x)    BOOST_PP_REPEAT_FROM_TO(1, BOOST_PP_ITERATION(), _dax_pp_repeat, x)
+#  define _dax_pp_repeat(z,n,x)   _dax_pp_repeat_(z,n,x)
+#  define _dax_pp_repeat_(z,n,x)  x(n)
+#  define _dax_pp_params___(x)    BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS(BOOST_PP_ITERATION(), T___, x)
 # endif // !(__cplusplus >= 201103L)
 
 #endif // !defined(DAX_DOXYGEN_ONLY)
