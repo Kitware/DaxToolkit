045580d173649ff464aed37a82059263ef7a59ae
Nikhil Shetty <nikhil.shetty@kitware.com>
2012-10-11 10:20:49 -0400

    Make changes to TestingGridGenerator based on new Cell API changes.

--- dax/cont/internal/testing/TestingGridGenerator.h
+++ dax/cont/internal/testing/TestingGridGenerator.h
@@ -88,11 +88,11 @@ public:
 
 
   //Get the cell at a given index
-  CellType GetCell(dax::Id cellId) const
+  CellType GetCell(dax::Id) const
   {
     typedef typename GridType::TopologyStructConstExecution TopoType;
     TopoType topo = this->Grid.PrepareForInput();
-    return CellType(topo,cellId);
+    return CellType(topo);
   }
 
   //get the cell connections (aka topology) at a given cell id
