3ff9819da4fd7517af6e3d333b0c99c9ef62805f
Robert Maynard <robert.maynard@kitware.com>
2012-11-07 12:56:48 -0500

    Add in support for constant cuda memory as DAX_EXC_CONSTANT_EXPORT
    static const int is not how you should declare constant device memory.
    So we have added a MACRO that properly declares memory as constant
    device only memory.

--- dax/worklet/internal/MarchingCubesTable.h
+++ dax/worklet/internal/MarchingCubesTable.h
@@ -17,13 +17,15 @@
 #ifndef __dax_worklet_internal_MarchingCubesTable_h
 #define __dax_worklet_internal_MarchingCubesTable_h
 
+#include <dax/internal/ExportMacros.h>
+
 namespace dax {
 namespace worklet{
 namespace internal{
 namespace marchingcubes{
 
 // ------------------------------------------------------------------- numFaces
-const int numFaces[256] =
+static DAX_EXEC_CONSTANT_EXPORT int numFaces[256] =
 { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 2,
   1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 3,
   1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 3,
@@ -50,7 +52,7 @@ struct TriangleCases
 // ................................................................... triTable
 // Edges to intersect.  Three at a time form a triangle. Comments at
 // end of line indicate case number (0->255) and base case number (0->15).
-static TriangleCases triTable[] = {
+static DAX_EXEC_CONSTANT_EXPORT TriangleCases triTable[] = {
   {{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}}, /* 0 0 */
   {{ 0, 3, 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}}, /* 1 1 */
   {{ 0, 9, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}}, /* 2 1 */
