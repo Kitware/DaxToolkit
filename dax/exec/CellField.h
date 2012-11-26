4a97e42e0aa2cc96aaf2560a96794ab8aafb2b01
Kenneth Moreland <kmorel@sandia.gov>
2012-11-26 16:13:15 -0700

    Add a CellField class.

---
+++ dax/exec/CellField.h
@@ -0,0 +1,78 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2012 Sandia Corporation.
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//=============================================================================
+#ifndef __dax_exec_CellField_h
+#define __dax_exec_CellField_h
+
+#include <dax/CellTraits.h>
+#include <dax/Types.h>
+
+namespace dax {
+namespace exec {
+
+/// \brief Holds field values for a cell of a particular type.
+///
+/// This class is really is a convienience wrapper around a dax::Tuple.
+///
+template<class CellTag, typename FieldType>
+class CellField
+{
+public:
+  const static int NUM_VERTICES = dax::CellTraits<CellTag>::NUM_VERTICES;
+  typedef dax::Tuple<FieldType, NUM_VERTICES> ValuesTupleType;
+
+  DAX_EXEC_EXPORT
+  CellField() {  }
+
+  DAX_EXEC_EXPORT
+  CellField(const ValuesTupleType &values)
+    : Values(values) {  }
+
+  // Although this copy constructor should be identical to the default copy
+  // constructor, we have noticed that NVCC's default copy constructor can
+  // incur a significant slowdown.
+  DAX_EXEC_EXPORT
+  CellField(const CellField &src)
+    : Values(src.Values) {  }
+
+  DAX_EXEC_EXPORT
+  FieldType GetValue(int vertexIndex) const
+  {
+    return this->Values[vertexIndex];
+  }
+
+  DAX_EXEC_EXPORT
+  void SetValue(int vertexIndex, FieldType fieldValue)
+  {
+    this->Values[vertexIndex] = fieldValue;
+  }
+
+  DAX_EXEC_EXPORT
+  const ValuesTupleType &GetValues() const { return this->Values; }
+
+  DAX_EXEC_EXPORT
+  void SetValues(const ValuesTupleType fieldValues)
+  {
+    this->Values = fieldValues;
+  }
+
+private:
+  ValuesTupleType Values;
+};
+
+}
+} // namespace dax::exec
+
+#endif //__dax_exec_CellField_h
---
+++ dax/exec/testing/UnitTestCellField.cxx
@@ -0,0 +1,208 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2012 Sandia Corporation.
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//=============================================================================
+#include <dax/exec/CellField.h>
+
+#include <dax/CellTag.h>
+#include <dax/VectorTraits.h>
+
+#include <dax/exec/internal/testing/TestingTopologyGenerator.h>
+
+#include <dax/internal/testing/Testing.h>
+
+namespace
+{
+
+dax::Id CreateUniqueOffset()
+{
+  static dax::Id nextOffset = 1;
+  return nextOffset++;
+}
+
+template<typename T>
+struct TestValueGenerator
+{
+  typedef typename dax::VectorTraits<T>::ComponentType ComponentType;
+
+  TestValueGenerator() : Offset(CreateUniqueOffset() * 10000) {  }
+  T operator[](dax::Id index) {
+    T value;
+    for (int component = 0;
+         component < dax::VectorTraits<T>::NUM_COMPONENTS;
+         component++)
+      {
+      dax::VectorTraits<T>::SetComponent(
+            value,
+            component,
+            static_cast<ComponentType>(this->Offset+10*index+component));
+      }
+    return value;
+  }
+private:
+  const dax::Id Offset;
+};
+
+template<class CellTag, typename T>
+struct CellFieldTests
+{
+  typedef dax::exec::CellField<CellTag, T> CVType;
+  const static dax::Id NUM_VERTICES = CVType::NUM_VERTICES;
+
+  static void TestSizesMatch()
+  {
+    std::cout << "  Testing sizes." << std::endl;
+
+    DAX_TEST_ASSERT(NUM_VERTICES == CVType::NUM_VERTICES,
+                    "Test has wrong number of vertices?");
+    DAX_TEST_ASSERT(
+          CVType::NUM_VERTICES == dax::CellTraits<CellTag>::NUM_VERTICES,
+          "CellField reports wrong number of points.");
+    DAX_TEST_ASSERT(NUM_VERTICES == CVType::ValuesTupleType::NUM_COMPONENTS,
+                    "CellField tuple has wrong number of components.");
+  }
+
+  static void TestSet1GetAll()
+  {
+    std::cout << "  Testing setting components individually, getting tuple."
+              << std::endl;
+
+    TestValueGenerator<T> values;
+    CVType vertices;
+
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      vertices.SetValue(vertexIndex, values[vertexIndex]);
+      }
+
+    const typename CVType::ValuesTupleType &valueTuple = vertices.GetValues();
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      DAX_TEST_ASSERT(valueTuple[vertexIndex] == values[vertexIndex],
+                      "Got wrong point index.");
+      }
+  }
+
+  static void TestSetAllGet1()
+  {
+    std::cout << "  Testing setting tuple, getting components individually."
+              << std::endl;
+
+    TestValueGenerator<T> values;
+    typename CVType::ValuesTupleType valueTuple;
+
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      valueTuple[vertexIndex] = values[vertexIndex];
+      }
+
+    CVType vertices;
+    vertices.SetValues(valueTuple);
+
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      DAX_TEST_ASSERT(
+            vertices.GetValue(vertexIndex) == values[vertexIndex],
+            "Got wrong point index.");
+      }
+  }
+
+  static void TestConstructors()
+  {
+    std::cout << "  Testing setting with constructors."
+              << std::endl;
+
+    TestValueGenerator<T> values;
+    typename CVType::ValuesTupleType valueTuple;
+
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      valueTuple[vertexIndex] = values[vertexIndex];
+      }
+
+    CVType vertices(valueTuple);
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      DAX_TEST_ASSERT(
+            vertices.GetValue(vertexIndex) == values[vertexIndex],
+            "Got wrong point index.");
+      }
+
+    CVType verticesCopy(vertices);
+    for (int vertexIndex = 0; vertexIndex < NUM_VERTICES; vertexIndex++)
+      {
+      DAX_TEST_ASSERT(
+            verticesCopy.GetValue(vertexIndex) == values[vertexIndex],
+            "Got wrong point index.");
+      }
+  }
+
+  static void TestAll()
+  {
+    TestSizesMatch();
+    TestSet1GetAll();
+    TestSetAllGet1();
+    TestConstructors();
+  }
+
+};
+
+template<class CellTag>
+struct TestCellFieldFunctorType
+{
+  template<typename T>
+  void operator()(T)
+  {
+    CellFieldTests<CellTag, T>::TestAll();
+  }
+};
+
+//struct TestCellFieldFunctor
+//{
+//  template<class TopologyGenType>
+//  void operator()(const TopologyGenType &topology)
+//  {
+//    typedef typename TopologyGenType::CellType CellType;
+//    typedef typename CellType::PointConnectionsType PointConnectionsType;
+
+//    dax::Id numCells = topology.GetNumberOfCells();
+//    for (dax::Id cellIndex = 0; cellIndex < numCells; cellIndex++)
+//      {
+//      CellType cell = topology.GetCell(cellIndex);
+//      DAX_TEST_ASSERT(cell.GetNumberOfPoints() == CellType::NUM_POINTS,
+//                      "Cell has wrong number of points");
+
+//      PointConnectionsType cellConnections = cell.GetPointIndices();
+//      PointConnectionsType expectedConnections =
+//          topology.GetCellConnections(cellIndex);
+//      DAX_TEST_ASSERT(test_equal(cellConnections, expectedConnections),
+//                      "Cell has unexpected connections.");
+//      }
+//  }
+//};
+
+void TestCellField()
+{
+  // Enable all tests once the TryAllTopologyTypes uses cell tags.
+//  dax::exec::internal::TryAllTopologyTypes(TestCellFieldFunctor());
+  dax::internal::Testing::TryAllTypes(
+        TestCellFieldFunctorType<dax::CellTagHexahedron>());
+}
+
+} // anonymous namespace
+
+int UnitTestCellField(int, char *[])
+{
+  return dax::internal::Testing::Run(TestCellField);
+}
