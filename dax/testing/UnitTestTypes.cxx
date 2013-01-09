4b18e243ce8c45eb6f03f9225aca25f858306f3b
Kenneth Moreland <kmorel@sandia.gov>
2013-01-08 17:33:16 -0700

    Allow floating point error in comparisons of vector type operations.
    Even though the vector operations in UnitTestTypes are all low precision
    integer that should be precisely represented in floating point values,
    the optimizations of Intel's icc compiler (which probably performs the
    computation in the compiler itself) are off by some small epsilon.

--- dax/testing/UnitTestTypes.cxx
+++ dax/testing/UnitTestTypes.cxx
@@ -29,54 +29,34 @@ template<> void TypeTest<dax::Vector2>()
   dax::Scalar s = 5;
 
   dax::Vector2 plus = a + b;
-  if ((plus[0] != 3) || (plus[1] != 6))
-    {
-    DAX_TEST_FAIL("Vectors do not add correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(plus, dax::make_Vector2(3, 6)),
+                  "Vectors do not add correctly.");
 
   dax::Vector2 minus = a - b;
-  if ((minus[0] != 1) || (minus[1] != 2))
-    {
-    DAX_TEST_FAIL("Vectors to not subtract correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(minus, dax::make_Vector2(1, 2)),
+                  "Vectors to not subtract correctly.");
 
   dax::Vector2 mult = a * b;
-  if ((mult[0] != 2) || (mult[1] != 8))
-    {
-    DAX_TEST_FAIL("Vectors to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector2(2, 8)),
+                  "Vectors to not multiply correctly.");
 
   dax::Vector2 div = a / b;
-  if ((div[0] != 2) || (div[1] != 2))
-    {
-    DAX_TEST_FAIL("Vectors to not divide correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(div, dax::make_Vector2(2, 2)),
+                  "Vectors to not divide correctly.");
 
   mult = s * a;
-  if ((mult[0] != 10) || (mult[1] != 20))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector2(10, 20)),
+                  "Vector and scalar to not multiply correctly.");
 
   mult = a * s;
-  if ((mult[0] != 10) || (mult[1] != 20))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector2(10, 20)),
+                  "Vector and scalar to not multiply correctly.");
 
-  if (a == b)
-    {
-    DAX_TEST_FAIL("operator== wrong");
-    }
-  if (!(a != b))
-    {
-    DAX_TEST_FAIL("operator!= wrong");
-    }
+  DAX_TEST_ASSERT(!(a == b), "operator== wrong");
+  DAX_TEST_ASSERT((a != b),"operator!= wrong");
 
-  if (dax::dot(a, b) != 10)
-    {
-    DAX_TEST_FAIL("dot(Vector2) wrong");
-    }
+  dax::Scalar d = dax::dot(a, b);
+  DAX_TEST_ASSERT(test_equal(d, dax::Scalar(10)), "dot(Vector2) wrong");
 }
 
 template<> void TypeTest<dax::Vector3>()
@@ -86,54 +66,34 @@ template<> void TypeTest<dax::Vector3>()
   dax::Scalar s = 5;
 
   dax::Vector3 plus = a + b;
-  if ((plus[0] != 3) || (plus[1] != 6) || (plus[2] != 9))
-    {
-    DAX_TEST_FAIL("Vectors do not add correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(plus, dax::make_Vector3(3, 6, 9)),
+                  "Vectors do not add correctly.");
 
   dax::Vector3 minus = a - b;
-  if ((minus[0] != 1) || (minus[1] != 2) || (minus[2] != 3))
-    {
-    DAX_TEST_FAIL("Vectors to not subtract correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(minus, dax::make_Vector3(1, 2, 3)),
+                  "Vectors to not subtract correctly.");
 
   dax::Vector3 mult = a * b;
-  if ((mult[0] != 2) || (mult[1] != 8) || (mult[2] != 18))
-    {
-    DAX_TEST_FAIL("Vectors to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector3(2, 8, 18)),
+                  "Vectors to not multiply correctly.");
 
   dax::Vector3 div = a / b;
-  if ((div[0] != 2) || (div[1] != 2) || (div[2] != 2))
-    {
-    DAX_TEST_FAIL("Vectors to not divide correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(div, dax::make_Vector3(2, 2, 2)),
+                  "Vectors to not divide correctly.");
 
   mult = s * a;
-  if ((mult[0] != 10) || (mult[1] != 20) || (mult[2] != 30))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector3(10, 20, 30)),
+                  "Vector and scalar to not multiply correctly.");
 
   mult = a * s;
-  if ((mult[0] != 10) || (mult[1] != 20) || (mult[2] != 30))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector3(10, 20, 30)),
+                  "Vector and scalar to not multiply correctly.");
 
-  if (a == b)
-    {
-    DAX_TEST_FAIL("operator== wrong");
-    }
-  if (!(a != b))
-    {
-    DAX_TEST_FAIL("operator!= wrong");
-    }
+  DAX_TEST_ASSERT(!(a == b), "operator== wrong");
+  DAX_TEST_ASSERT((a != b),"operator!= wrong");
 
-  if (dax::dot(a, b) != 28)
-    {
-    DAX_TEST_FAIL("dot(Vector3) wrong");
-    }
+  dax::Scalar d = dax::dot(a, b);
+  DAX_TEST_ASSERT(test_equal(d, dax::Scalar(28)), "dot(Vector3) wrong");
 }
 
 template<> void TypeTest<dax::Vector4>()
@@ -143,54 +103,34 @@ template<> void TypeTest<dax::Vector4>()
   dax::Scalar s = 5;
 
   dax::Vector4 plus = a + b;
-  if ((plus[0] != 3) || (plus[1] != 6) || (plus[2] != 9) || (plus[3] != 12))
-    {
-    DAX_TEST_FAIL("Vectors do not add correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(plus, dax::make_Vector4(3, 6, 9, 12)),
+                  "Vectors do not add correctly.");
 
   dax::Vector4 minus = a - b;
-  if ((minus[0] != 1) || (minus[1] != 2) || (minus[2] != 3) || (minus[3] != 4))
-    {
-    DAX_TEST_FAIL("Vectors to not subtract correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(minus, dax::make_Vector4(1, 2, 3, 4)),
+                  "Vectors to not subtract correctly.");
 
   dax::Vector4 mult = a * b;
-  if ((mult[0] != 2) || (mult[1] != 8) || (mult[2] != 18) || (mult[3] != 32))
-    {
-    DAX_TEST_FAIL("Vectors to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector4(2, 8, 18, 32)),
+                  "Vectors to not multiply correctly.");
 
   dax::Vector4 div = a / b;
-  if ((div[0] != 2) || (div[1] != 2) || (div[2] != 2) || (div[3] != 2))
-    {
-    DAX_TEST_FAIL("Vectors to not divide correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(div, dax::make_Vector4(2, 2, 2, 2)),
+                  "Vectors to not divide correctly.");
 
   mult = s * a;
-  if ((mult[0] != 10) || (mult[1] != 20) || (mult[2] != 30) || (mult[3] != 40))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector4(10, 20, 30, 40)),
+                  "Vector and scalar to not multiply correctly.");
 
   mult = a * s;
-  if ((mult[0] != 10) || (mult[1] != 20) || (mult[2] != 30) || (mult[3] != 40))
-    {
-    DAX_TEST_FAIL("Vector and scalar to not multiply correctly.");
-    }
+  DAX_TEST_ASSERT(test_equal(mult, dax::make_Vector4(10, 20, 30, 40)),
+                  "Vector and scalar to not multiply correctly.");
 
-  if (a == b)
-    {
-    DAX_TEST_FAIL("operator== wrong");
-    }
-  if (!(a != b))
-    {
-    DAX_TEST_FAIL("operator!= wrong");
-    }
+  DAX_TEST_ASSERT(!(a == b), "operator== wrong");
+  DAX_TEST_ASSERT((a != b),"operator!= wrong");
 
-  if (dax::dot(a, b) != 60)
-    {
-    DAX_TEST_FAIL("dot(Vector4) wrong");
-    }
+  dax::Scalar d = dax::dot(a, b);
+  DAX_TEST_ASSERT(test_equal(d, dax::Scalar(60)), "dot(Vector4) wrong");
 }
 
 template<> void TypeTest<dax::Id3>()
