33a06b1426da8e02afcd5b6a8b916bcd141660a7
Kenneth Moreland <kmorel@sandia.gov>
2013-01-13 11:30:29 -0700

    Add copy constructor to Tuple.
    For some reason, the icc compiler, with optimization on, was having
    problems with its own default copy constructor.

--- dax/Types.h
+++ dax/Types.h
@@ -223,6 +223,14 @@ public:
       this->Components[i]=values[i];
       }
     }
+  DAX_EXEC_CONT_EXPORT
+  Tuple(const Tuple<ComponentType, Size> &src)
+  {
+    for (int i = 0; i < NUM_COMPONENTS; i++)
+      {
+      this->Components[i] = src[i];
+      }
+  }
 
   DAX_EXEC_CONT_EXPORT const ComponentType &operator[](int idx) const {
     return this->Components[idx];
@@ -275,6 +283,11 @@ public:
     this->Components[0] = x;
     this->Components[1] = y;
   }
+  DAX_EXEC_CONT_EXPORT
+  Tuple(const Tuple<ComponentType, NUM_COMPONENTS> &src)
+  {
+    internal::copy_vector<NUM_COMPONENTS>()(this->Components, src.Components);
+  }
 
   DAX_EXEC_CONT_EXPORT const ComponentType &operator[](int idx) const {
     return this->Components[idx];
@@ -322,6 +335,11 @@ public:
     this->Components[0] = x;
     this->Components[1] = y;
     this->Components[2] = z;
+  }
+  DAX_EXEC_CONT_EXPORT
+  Tuple(const Tuple<ComponentType, NUM_COMPONENTS> &src)
+  {
+    internal::copy_vector<NUM_COMPONENTS>()(this->Components, src.Components);
   }
 
   DAX_EXEC_CONT_EXPORT const ComponentType &operator[](int idx) const {
@@ -371,6 +389,11 @@ public:
     this->Components[1] = y;
     this->Components[2] = z;
     this->Components[3] = w;
+  }
+  DAX_EXEC_CONT_EXPORT
+  Tuple(const Tuple<ComponentType, NUM_COMPONENTS> &src)
+  {
+    internal::copy_vector<NUM_COMPONENTS>()(this->Components, src.Components);
   }
 
   DAX_EXEC_CONT_EXPORT const ComponentType &operator[](int idx) const {
