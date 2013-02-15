f9c8f7e7e3dfd753ad7f0f42a25b21eea8744310
Kenneth Moreland <kmorel@sandia.gov>
2013-02-15 13:02:59 -0700

    Add synchronization and custom timer for CUDA device.

--- dax/cuda/cont/internal/DeviceAdapterAlgorithmCuda.h
+++ dax/cuda/cont/internal/DeviceAdapterAlgorithmCuda.h
@@ -26,6 +26,8 @@
 // Here are the actual implementation of the algorithms.
 #include <dax/thrust/cont/internal/DeviceAdapterAlgorithmThrust.h>
 
+#include <cuda.h>
+
 namespace dax {
 namespace cont {
 namespace internal {
@@ -34,10 +36,67 @@ template<>
 struct DeviceAdapterAlgorithm<dax::cuda::cont::DeviceAdapterTagCuda>
     : public dax::thrust::cont::internal::DeviceAdapterAlgorithmThrust<
           dax::cuda::cont::DeviceAdapterTagCuda>
-{  };
+{
+
+  DAX_CONT_EXPORT static void Synchronize()
+  {
+    cudaError_t error = cudaDeviceSynchronize();
+  }
+
+};
 
 }
 }
 } // namespace dax::cont::internal
+
+namespace dax {
+namespace cont {
+
+// Add prototype for Timer template, which might not be defined yet.
+template<class DeviceAdapter> class Timer;
+
+/// CUDA contains its own high resolution timer.
+///
+template<>
+class Timer<dax::cuda::cont::DeviceAdapterTagCuda>
+{
+public:
+  DAX_CONT_EXPORT Timer()
+  {
+    cudaEventCreate(&this->StartEvent);
+    cudaEventCreate(&this->EndEvent);
+    this->Reset();
+  }
+  DAX_CONT_EXPORT ~Timer()
+  {
+    cudaEventDestroy(this->StartEvent);
+    cudaEventDestroy(this->EndEvent);
+  }
+
+  DAX_CONT_EXPORT void Reset()
+  {
+    cudaEventRecord(this->StartEvent, 0);
+  }
+
+  DAX_CONT_EXPORT dax::Scalar GetElapsedTime()
+  {
+    cudaEventRecord(this->EndEvent, 0);
+    cudaEventSynchronize(this->EndEvent);
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, this->StartEvent, this->EndEvent);
+    return static_cast<dax::Scalar>(elapsedTime);
+  }
+
+private:
+  // Copying CUDA events is problematic.
+  Timer(const Timer<dax::cuda::cont::DeviceAdapterTagCuda> &);
+  void operator=(const Timer<dax::cuda::cont::DeviceAdapterTagCuda> &);
+
+  cudaEvent_t StartEvent;
+  cudaEvent_t EndEvent;
+};
+
+}
+} // namespace dax::cont
 
 #endif //__dax_cuda_cont_internal_DeviceAdapterAlgorithmCuda_h
