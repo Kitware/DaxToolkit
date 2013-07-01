b42f95b2cf452bcd9b396b7a21a3206f13a5f3bf
Robert Maynard <robert.maynard@kitware.com>
2013-07-01 14:40:34 -0400

    Installed Dax 32/64 types can be overridden by project including dax.
    Before this Dax at configure time would determine if it was in 32 or 64 bit mode
    and configure everything at that point. This meant that if Dax was installed
    on a machine, consuming application couldn't override the size of Scalar or Id.
    For Dax to be a proper header only library, these things should be set by
    the consuming application.
    
    To make it easier for consuming application to set if Dax should use 32 or 64bit
    data types, we now provide ConfigureFor32 and ConfigureFor64 header files.

---
+++ dax/internal/ConfigureFor64.h
@@ -0,0 +1,38 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//  Copyright 2012 Sandia Corporation.
+//  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+//  the U.S. Government retains certain rights in this software.
+//
+//=============================================================================
+//This header can be used by external application that are consuming Dax
+//to define if Dax should be set to use 64bit data types. If you need to
+//customize more of the dax type system, or what Device Adapters
+//need to be included look at dax/internal/Configure.h for all defines that
+//you can over-ride.
+
+#ifndef __dax_internal_Configure32_h
+#define __dax_internal_Configure32_h
+
+#define DAX_USE_DOUBLE_PRECISION
+#define DAX_USE_64BIT_IDS
+
+#define DAX_SIZE_DOUBLE 8
+
+#ifdef _WIN32
+# define DAX_SIZE_LONG_LONG 8
+#else
+# define DAX_SIZE_LONG 8
+#endif
+
+#include <dax/internal/Configure.h>
+
+#endif
