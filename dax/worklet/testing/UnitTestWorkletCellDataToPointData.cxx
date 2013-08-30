81b3b5c6301c46f0ad8c9d9136229dc3dfb0c89c
Robert Maynard <robert.maynard@kitware.com>
2013-08-30 15:25:48 -0400

    Update UnitTestWorkletCellDataToPointData to work on all device adapters.

--- dax/worklet/testing/UnitTestWorkletCellDataToPointData.cxx
+++ dax/worklet/testing/UnitTestWorkletCellDataToPointData.cxx
@@ -14,11 +14,6 @@
 //
 //=============================================================================
 
-// These macros help tease out when the default template arguments to
-// ArrayHandle are inappropriately used.
-#define DAX_ARRAY_CONTAINER_CONTROL DAX_ARRAY_CONTAINER_CONTROL_ERROR
-#define DAX_DEVICE_ADAPTER DAX_DEVICE_ADAPTER_ERROR
-
 #include <dax/cont/testing/TestingGridGenerator.h>
 #include <dax/cont/testing/Testing.h>
 
@@ -35,15 +30,13 @@
 #include <dax/cont/ReduceKeysValues.h>
 #include <dax/cont/Scheduler.h>
 
+#include <iostream>
 #include <algorithm>
 #include <vector>
 
 namespace {
 
 const dax::Id DIM = 3;
-
-typedef dax::cont::ArrayContainerControlTagBasic ArrayContainer;
-typedef dax::cont::DeviceAdapterTagSerial DeviceAdapter;
 
 //-----------------------------------------------------------------------------
 template<typename TestGrid>
@@ -61,10 +54,9 @@ void verifyPointData(const TestGrid &grid,
 
   for (dax::Id cellIndex = 0; cellIndex < grid->GetNumberOfCells(); cellIndex++)
     {
-    dax::exec::CellVertices<CellTag> cellConnections =
+    dax::cont::testing::CellConnections<CellTag> cellConnections =
       grid.GetCellConnections(cellIndex);
 
-    std::cout << "Cell " << cellIndex << "(" << cellData[cellIndex] << ") : ";
     for (int vertexIndex = 0;
          vertexIndex < cellConnections.NUM_VERTICES;
          vertexIndex++)
@@ -72,9 +64,7 @@ void verifyPointData(const TestGrid &grid,
       dax::Id pointIndex = cellConnections[vertexIndex];
       numConnections[pointIndex]++;
       pointDataSums[pointIndex] += cellData[cellIndex];
-      std::cout << "{" << cellConnections[vertexIndex] << ", " << computedPointData[pointIndex] << "}, ";
       }
-    std::cout << std::endl;
     }
 
   for (dax::Id pointIndex = 0;
@@ -84,23 +74,11 @@ void verifyPointData(const TestGrid &grid,
     dax::Scalar expectedValue =
       pointDataSums[pointIndex] / numConnections[pointIndex];
     dax::Scalar computedValue = computedPointData[pointIndex];
-    std::cout << "pointDataSums: " << pointDataSums[pointIndex] << std::endl;
-    std::cout << "numConnections: " << numConnections[pointIndex] << std::endl;
-    std::cout << "Expected value: " << expectedValue << std::endl;
-    std::cout << "Computed value: " << computedValue << std::endl;
     DAX_TEST_ASSERT(test_equal(computedValue, expectedValue),
                     "Got bad average at point");
     }
 }
-template<class IteratorType>
-void PrintArray(IteratorType beginIter, IteratorType endIter)
-{
-  for (IteratorType iter = beginIter; iter != endIter; iter++)
-    {
-    std::cout << " " << *iter;
-    }
-  std::cout << std::endl;
-}
+
 //-----------------------------------------------------------------------------
 struct TestCellDataToPointDataWorklet
 {
@@ -110,8 +88,7 @@ struct TestCellDataToPointDataWorklet
   {
     typedef typename GridType::CellTag CellTag;
 
-    dax::cont::testing::TestGrid<GridType, ArrayContainer, DeviceAdapter>
-      grid(DIM);
+    dax::cont::testing::TestGrid<GridType> grid(DIM);
 
 
     std::vector<dax::Scalar> field(grid->GetNumberOfCells());
@@ -122,27 +99,23 @@ struct TestCellDataToPointDataWorklet
       field[cellIndex] = cellIndex;
       }
 
-    dax::cont::ArrayHandle<dax::Scalar,ArrayContainer,DeviceAdapter> fieldHandle
-        = dax::cont::make_ArrayHandle(field, ArrayContainer(), DeviceAdapter());
+    dax::cont::ArrayHandle<dax::Scalar> fieldHandle
+                                        = dax::cont::make_ArrayHandle(field);
 
-    dax::cont::ArrayHandleConstant<dax::Id,DeviceAdapter> keyGenCounts =
-      dax::cont::make_ArrayHandleConstant(dax::CellTraits<CellTag>::NUM_VERTICES,
-                                          grid->GetNumberOfCells(),
-                                          DeviceAdapter());
+    dax::cont::ArrayHandleConstant<dax::Id> keyGenCounts =
+      dax::cont::make_ArrayHandleConstant(
+            dax::CellTraits<CellTag>::NUM_VERTICES, grid->GetNumberOfCells());
 
-    dax::cont::ArrayHandle<dax::Id,ArrayContainer,DeviceAdapter> keyHandle;
-    dax::cont::ArrayHandle<dax::Scalar,ArrayContainer,DeviceAdapter> valueHandle;
-
-    dax::cont::ArrayHandle<dax::Scalar,ArrayContainer,DeviceAdapter>
-      resultHandle;
+    dax::cont::ArrayHandle<dax::Id> keyHandle;
+    dax::cont::ArrayHandle<dax::Scalar> valueHandle;
+    dax::cont::ArrayHandle<dax::Scalar> resultHandle;
 
     std::cout << "Running CellDataToPointDataGenerateKeys worklet" << std::endl;
-    dax::cont::Scheduler<DeviceAdapter> scheduler;
+    dax::cont::Scheduler< > scheduler;
 
     dax::cont::GenerateKeysValues<
-      dax::worklet::CellDataToPointDataGenerateKeys,
-      dax::cont::ArrayHandleConstant<dax::Id,DeviceAdapter> >
-          generateKeys(keyGenCounts);
+        dax::worklet::CellDataToPointDataGenerateKeys,
+        dax::cont::ArrayHandleConstant<dax::Id> > generateKeys(keyGenCounts);
 
     scheduler.Invoke(generateKeys,
                      grid.GetRealGrid(),
@@ -156,59 +129,13 @@ struct TestCellDataToPointDataWorklet
 
     dax::cont::ReduceKeysValues<
       dax::worklet::CellDataToPointDataReduceKeys,
-      dax::cont::ArrayHandle<dax::Id,ArrayContainer,DeviceAdapter> >
-        reduceKeys(keyHandle, CD2PD);
+      dax::cont::ArrayHandle<dax::Id> > reduceKeys(keyHandle, CD2PD);
 
-/*    std::vector<dax::Scalar> keyData(keyHandle.GetNumberOfValues());
-    keyHandle.CopyInto(keyData.begin());
-
-    std::cout << "keyData: ";
-    for(int iCtr = 0; iCtr < keyData.size(); iCtr++)
-        std::cout << keyData[iCtr] << ", ";
-    std::cout << std::endl;
-    std::cout << std::endl;
-
-    std::vector<dax::Scalar> valueData(valueHandle.GetNumberOfValues());
-    valueHandle.CopyInto(valueData.begin());
-
-    std::cout << "valueData: ";
-    for(int iCtr = 0; iCtr < valueData.size(); iCtr++)
-        std::cout << valueData[iCtr] << ", ";
-    std::cout << std::endl;
-    std::cout << std::endl;
-*/
-    scheduler.Invoke(reduceKeys,
-                     valueHandle,
-                     resultHandle);
-
-/*
-    std::cout << "CountSize: " << resultHandle.GetReductionCounts().GetNumberOfValues() << std::endl;
-    std::cout << "OffsetSize: " << resultHandle.GetReductionOffsets().GetNumberOfValues() << std::endl;
-    std::cout << "IndexSize: " << resultHandle.GetReductionIndices().GetNumberOfValues() << std::endl;
-    std::cout << "Counts: ";
-    PrintArray(
-          reduceKeys.GetReductionCounts().GetPortalConstControl().GetIteratorBegin(),
-          reduceKeys.GetReductionCounts().GetPortalConstControl().GetIteratorEnd());
-
-    std::vector<dax::Scalar> valueData(valueHandle.GetNumberOfValues());
-    valueHandle.CopyInto(valueData.begin());
-
-    std::cout << "valueData: ";
-    for(int iCtr = 0; iCtr < valueData.size(); iCtr++)
-        std::cout << valueData[iCtr] << ", ";
-    std::cout << std::endl;
-    std::cout << std::endl;
-  */
+    scheduler.Invoke(reduceKeys, valueHandle, resultHandle);
 
     std::cout << "Checking result" << std::endl;
     std::vector<dax::Scalar> pointData(resultHandle.GetNumberOfValues());
     resultHandle.CopyInto(pointData.begin());
-
-    std::cout << "pointData: ";
-    for(unsigned int iCtr = 0; iCtr < pointData.size(); iCtr++)
-        std::cout << pointData[iCtr] << ", ";
-    std::cout << std::endl;
-    std::cout << std::endl;
 
     verifyPointData(grid, field, pointData);
   }
@@ -218,9 +145,7 @@ struct TestCellDataToPointDataWorklet
 void TestCellDataToPointData()
   {
   dax::cont::testing::GridTesting::TryAllGridTypes(
-        TestCellDataToPointDataWorklet(),
-        ArrayContainer(),
-        DeviceAdapter());
+                                           TestCellDataToPointDataWorklet());
   }
 
 
