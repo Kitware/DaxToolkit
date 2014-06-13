3979221db1275819a4e31b9815532a833fed5b1c
Robert Maynard <robert.maynard@kitware.com>
2014-06-13 11:37:39 -0400

    Simplify GetNthType by using boost function_types.
    Not only does this simplify the implementation, it doesn't crash the
    VisualStudio 2013 compiler like the original implementation did.

--- dax/internal/GetNthType.h
+++ dax/internal/GetNthType.h
@@ -13,11 +13,11 @@
 //  the U.S. Government retains certain rights in this software.
 //
 //=============================================================================
-#if !defined(BOOST_PP_IS_ITERATING)
-
 # ifndef __dax__internal__GetNthType_h
 # define __dax__internal__GetNthType_h
-# if defined(DAX_DOXYGEN_ONLY)
+
+#include <boost/function_types/components.hpp>
+#include <boost/mpl/at.hpp>
 
 namespace dax { namespace internal {
 
@@ -33,45 +33,14 @@ namespace dax { namespace internal {
 template <unsigned int N, typename TypeSequence>
 struct GetNthType
 {
+private:
+  typedef typename boost::function_types::components<TypeSequence>::type ParameterTypes;
+public:
   /// Type at index \c N of \c TypeSequence
-  typedef type_of_Nth_element type;
+  typedef typename boost::mpl::at_c<ParameterTypes, N>::type type;
+
 };
 
 }} // namespace dax::internal
 
-# else // !defined(DAX_DOXYGEN_ONLY)
-
-# include <dax/internal/Configure.h>
-
-# ifndef DAX_USE_VARIADIC_TEMPLATE
-#  include <dax/internal/ParameterPackCxx03.h>
-# endif // !DAX_USE_VARIADIC_TEMPLATE
-
-namespace dax { namespace internal {
-
-// Primary template is not defined.
-template <unsigned int N, typename TypeSequence> struct GetNthType;
-
-// Specialize for function types of each arity.
-template <typename T0> struct GetNthType<0, T0()> { typedef T0 type; };
-# ifdef DAX_USE_VARIADIC_TEMPLATE
-template <typename T0, typename T1, typename... T> struct GetNthType<0, T0(T1,T...)> { typedef T0 type; };
-template <typename T0, typename T1, typename... T> struct GetNthType<1, T0(T1,T...)> { typedef T1 type; };
-template <unsigned int N, typename T0, typename T1, typename... T> struct GetNthType<N, T0(T1,T...)> { typedef typename GetNthType<N-1,T0(T...)>::type type; };
-# else // !DAX_USE_VARIADIC_TEMPLATE
-#  define BOOST_PP_ITERATION_PARAMS_1 (3, (1, 10, <dax/internal/GetNthType.h>))
-#  include BOOST_PP_ITERATE()
-# endif // !DAX_USE_VARIADIC_TEMPLATE
-
-}} // namespace dax::internal
-
-# endif // !defined(DAX_DOXYGEN_ONLY)
-# endif //__dax__internal__GetNthType_h
-
-#else // defined(BOOST_PP_IS_ITERATING)
-
-template <typename T0, typename T1 _dax_pp_comma _dax_pp_typename___T> struct GetNthType<0, T0(T1 _dax_pp_comma _dax_pp_T___)> { typedef T0 type; };
-template <typename T0, typename T1 _dax_pp_comma _dax_pp_typename___T> struct GetNthType<1, T0(T1 _dax_pp_comma _dax_pp_T___)> { typedef T1 type; };
-template <unsigned int N, typename T0, typename T1 _dax_pp_comma _dax_pp_typename___T> struct GetNthType<N, T0(T1 _dax_pp_comma _dax_pp_T___)> { typedef typename GetNthType<N-1,T0(_dax_pp_T___)>::type type; };
-
-#endif // defined(BOOST_PP_IS_ITERATING)
+#endif
